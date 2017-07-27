51d233bca8497d7b809208eb14067040b5be7361
Jordi Ballester <jordi.ballester@eficent.com>
2017-07-27 18:17:19 +0200

    init branch

---
+++ rma/models/stock_warehouse.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+from openerp import api, fields, models
+
+
+class StockWarehouse(models.Model):
+
+    _inherit = "stock.warehouse"
+
+    lot_rma_id = fields.Many2one('stock.location', 'RMA Location')
+    rma_cust_out_type_id = fields.Many2one('stock.picking.type',
+                                           'RMA Customer out Type')
+    rma_sup_out_type_id = fields.Many2one('stock.picking.type',
+                                          'RMA Supplier out Type')
+    rma_cust_in_type_id = fields.Many2one('stock.picking.type',
+                                          'RMA Customer in Type')
+    rma_sup_in_type_id = fields.Many2one('stock.picking.type',
+                                         'RMA Supplier in Type')
+
+
+class StockLocationRoute(models.Model):
+    _inherit = "stock.location.route"
+
+    rma_selectable = fields.Boolean(string="Selectable on RMA Lines")
