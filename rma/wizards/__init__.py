744db0866f3687e4a0fa87347b847a21f5307772
Jordi Ballester <jordi.ballester@eficent.com>
2017-07-27 18:17:19 +0200

    init branch

---
+++ rma/wizards/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+from . import rma_add_stock_move
+from . import rma_make_picking
+from . import stock_config_settings
+from . import rma_order_line_make_supplier_rma
