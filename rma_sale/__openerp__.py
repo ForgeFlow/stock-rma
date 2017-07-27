1a5b49b2dd1e14c1a5f123a58808176c0c2e4be9
Jordi Ballester <jordi.ballester@eficent.com>
2017-07-27 18:17:19 +0200

    init branch

---
+++ rma_sale/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+{
+    'name': 'RMA Sale',
+    'version': '9.0.1.0.0',
+    'license': 'LGPL-3',
+    'category': 'RMA',
+    'summary': 'Links RMA with Sales Orders',
+    'author': "Eficent",
+    'website': 'http://www.github.com/OCA/rma',
+    'depends': ['rma_account', 'sale_stock'],
+    'data': ['views/rma_order_view.xml',
+             'views/rma_operation_view.xml',
+             'views/sale_order_view.xml',
+             'wizards/rma_order_line_make_sale_order_view.xml',
+             'wizards/rma_add_sale.xml',
+             'views/rma_order_line_view.xml'],
+    'installable': True,
+    'auto_install': True,
+}
