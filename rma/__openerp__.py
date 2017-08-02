5b64a08b0005bc4a5bf95b64af3727d3ee7ff4d9
lreficent <lois.rilo@eficent.com>
2017-08-02 14:04:18 +0200

    [9.0] rma: remove rma.rule and add that setting to product.category

--- rma/__openerp__.py
+++ rma/__openerp__.py
@@ -6,12 +6,12 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 {
-    'name': 'rma',
+    'name': 'RMA (Return Merchandise Authorization)',
     'version': '9.0.1.0.0',
     'license': 'LGPL-3',
     'category': 'RMA',
     'summary': 'Introduces the return merchandise authorization (RMA) process '
-               'in odoo',
+               'in Odoo',
     'author': "Akretion, Camptocamp, Eezee-it, MONK Software, Vauxoo, Eficent,"
               "Odoo Community Association (OCA)",
     'website': 'http://www.github.com/OCA/rma',
@@ -22,19 +22,16 @@
              ],
     'data': ['security/rma.xml',
              'security/ir.model.access.csv',
-
              'data/rma_sequence.xml',
              'data/stock_data.xml',
              'data/rma_operation.xml',
              'views/rma_order_view.xml',
              'views/rma_operation_view.xml',
-             'views/rma_rule_view.xml',
              'views/rma_order_line_view.xml',
              'views/stock_view.xml',
              'views/stock_warehouse.xml',
              'views/product_view.xml',
              'views/procurement_view.xml',
-             'views/res_company_view.xml',
              'wizards/rma_make_picking_view.xml',
              'wizards/rma_add_stock_move_view.xml',
              'wizards/stock_config_settings.xml',
--- rma/models/__init__.py
+++ rma/models/__init__.py
@@ -4,12 +4,12 @@
 # © 2013 Camptocamp
 # © 2009-2013 Akretion,
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
 from . import rma_order
 from . import rma_order_line
 from . import rma_operation
-from . import rma_rule
 from . import stock
 from . import stock_warehouse
 from . import product
+from . import product_category
 from . import procurement
-from . import res_company
