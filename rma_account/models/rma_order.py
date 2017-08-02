7c01a2f4782f0e06071366cf24294f24fb2d0da0
lreficent <lois.rilo@eficent.com>
2017-08-02 17:05:58 +0200

    [9.0][FIX] rma: * fix assignment of moves. * default qty in rma lines. * remove account dependency. * test and flake8 fixes.

--- rma_account/models/rma_order.py
+++ rma_account/models/rma_order.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
-from openerp import _, api, fields, models
-from openerp.addons import decimal_precision as dp
-from openerp.exceptions import UserError
-from dateutil.relativedelta import relativedelta
-from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
-from datetime import datetime
+from openerp import api, fields, models
 
 
 class RmaOrder(models.Model):
