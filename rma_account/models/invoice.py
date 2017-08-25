90ff5a613dbd427046ed57079a8b41efd93a7ef8
lreficent <lois.rilo@eficent.com>
2017-08-25 12:22:53 +0200

    [9.0][IMP] rma_account: * remove unneded copy attributes. * simplify action_view methods. * fix wrong naming. * fix misplaced views. * fix wrong count and view actions for rma.orders in invoices. * fix error when installing the module. * remove unneded data update when preparing rma lines from invoice lines. * minor extra fixes.

--- rma_account/models/invoice.py
+++ rma_account/models/invoice.py
@@ -6,34 +6,25 @@ from openerp import api, fields, models
 
 
 class AccountInvoice(models.Model):
-
     _inherit = "account.invoice"
 
     @api.one
     def _compute_rma_count(self):
-        rma_list = []
-        for invl in self.invoice_line_ids:
-            for rmal in invl.rma_line_ids:
-                rma_list.append(rmal.rma_id.id)
-        self.rma_count = len(list(set(rma_list)))
+        for inv in self:
+            rmas = self.mapped('invoice_line_ids.rma_line_ids.rma_id')
+            inv.rma_count = len(rmas)
 
-    rma_count = fields.Integer(compute=_compute_rma_count,
-                               string='# of RMA',
-                               copy=False)
+    rma_count = fields.Integer(
+        compute=_compute_rma_count, string='# of RMA')
 
     @api.multi
     def action_view_rma_supplier(self):
         action = self.env.ref('rma.action_rma_supplier')
         result = action.read()[0]
-        rma_list = []
-        for invl in self.invoice_line_ids:
-            for rmal in invl.rma_line_ids:
-                rma_list.append(rmal.rma_id.id)
-        self.rma_count = len(list(set(rma_list)))
+        rma_list = self.mapped('invoice_line_ids.rma_line_ids.rma_id').ids
         # choose the view_mode accordingly
         if len(rma_list) != 1:
-            result['domain'] = "[('id', 'in', " + \
-                               str(rma_list) + ")]"
+            result['domain'] = [('id', 'in', rma_list)]
         elif len(rma_list) == 1:
             res = self.env.ref('rma.view_rma_supplier_form', False)
             result['views'] = [(res and res.id or False, 'form')]
@@ -41,18 +32,13 @@ class AccountInvoice(models.Model):
         return result
 
     @api.multi
-    def action_view_rma(self):
+    def action_view_rma_customer(self):
         action = self.env.ref('rma.action_rma_customer')
         result = action.read()[0]
-        rma_list = []
-        for invl in self.invoice_line_ids:
-            for rmal in invl.rma_line_ids:
-                rma_list.append(rmal.rma_id.id)
-        self.rma_count = len(list(set(rma_list)))
+        rma_list = self.mapped('invoice_line_ids.rma_line_ids.rma_id').ids
         # choose the view_mode accordingly
         if len(rma_list) != 1:
-            result['domain'] = "[('id', 'in', " + \
-                               str(rma_list) + ")]"
+            result['domain'] = [('id', 'in', rma_list)]
         elif len(rma_list) == 1:
             res = self.env.ref('rma.view_rma_form', False)
             result['views'] = [(res and res.id or False, 'form')]
@@ -61,20 +47,16 @@ class AccountInvoice(models.Model):
 
 
 class AccountInvoiceLine(models.Model):
-
     _inherit = "account.invoice.line"
 
     @api.multi
     def _compute_rma_count(self):
-        rma_list = []
         for invl in self:
-            for rmal in invl.rma_line_ids:
-                rma_list.append(rmal.rma_id.id)
-            invl.rma_count = len(list(set(rma_list)))
+            rma_lines = invl.mapped('rma_line_ids')
+            invl.rma_line_count = len(rma_lines)
 
-    rma_count = fields.Integer(compute=_compute_rma_count,
-                               string='# of RMA',
-                               copy=False)
+    rma_line_count = fields.Integer(
+        compute=_compute_rma_count, string='# of RMA')
     rma_line_ids = fields.One2many(
         comodel_name='rma.order.line', inverse_name='invoice_line_id',
         string="RMA", readonly=True,
