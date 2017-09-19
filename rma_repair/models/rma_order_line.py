103b353cf048252715c46895f2ad5559adef9b85
Jordi Ballester <jordi.ballester@eficent.com>
2017-09-19 13:58:49 +0200

    add rma_repair

---
+++ rma_repair/models/rma_order_line.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+from openerp import api, fields, models
+from openerp.addons import decimal_precision as dp
+
+
+class RmaOrderLine(models.Model):
+    _inherit = "rma.order.line"
+
+    @api.one
+    @api.depends('repair_ids', 'repair_ids.state')
+    def _compute_qty_to_repair(self):
+        if self.repair_type == 'no':
+            self.qty_to_repair = 0.0
+        elif self.repair_type == 'ordered':
+            qty = self._get_rma_repaired_qty()
+            self.qty_to_repair = self.product_qty - qty
+        elif self.repair_type == 'received':
+            qty = self._get_rma_repaired_qty()
+            self.qty_to_repair = self.qty_received - qty
+        else:
+            self.qty_to_repair = 0.0
+
+    @api.one
+    @api.depends('repair_ids', 'repair_type', 'repair_ids.state')
+    def _compute_qty_repaired(self):
+        self.qty_repaired = self._get_rma_repaired_qty()
+
+    @api.multi
+    def _compute_repair_count(self):
+        for line in self:
+            line.repair_count = len(line.repair_ids)
+
+    repair_ids = fields.One2many(
+        comodel_name='mrp.repair', inverse_name='rma_line_id',
+        string='Repair Orders', readonly=True,
+        states={'draft': [('readonly', False)]}, copy=False)
+    qty_to_repair = fields.Float(
+        string='Qty To Sell', copy=False,
+        digits=dp.get_precision('Product Unit of Measure'),
+        readonly=True, compute=_compute_qty_to_repair,
+        store=True)
+    qty_repaired = fields.Float(
+        string='Qty Sold', copy=False,
+        digits=dp.get_precision('Product Unit of Measure'),
+        readonly=True, compute=_compute_qty_repaired,
+        store=True)
+    repair_type = fields.Selection(selection=[
+        ('no', 'Not required'), ('ordered', 'Based on Ordered Quantities'),
+        ('received', 'Based on Received Quantities')],
+        string="Repair Policy", default='no', required=True)
+    repair_count = fields.Integer(
+        compute=_compute_repair_count, string='# of Repairs')
+
+    @api.multi
+    def action_view_repair_order(self):
+        action = self.env.ref('mrp_repair.action_repair_order_tree')
+        result = action.read()[0]
+        result['domain'] = [('id', 'in', self.repair_ids.ids)]
+        return result
+
+    @api.multi
+    def _get_rma_repaired_qty(self):
+        self.ensure_one()
+        qty = 0.0
+        for repair in self.repair_ids.filtered(
+                lambda p: p.state != 'cancel'):
+            repair_qty = self.env['product.uom']._compute_qty_obj(
+                self.uom_id,
+                repair.product_uom_qty,
+                repair.product_uom,
+            )
+            qty += repair_qty
+        return qty
