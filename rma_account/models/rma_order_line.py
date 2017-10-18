8822653d193d35c60787b382026d2fdad58ca97e
lreficent <lois.rilo@eficent.com>
2017-10-18 16:47:35 +0200

    rma_account: adapt.

--- rma_account/models/rma_order_line.py
+++ rma_account/models/rma_order_line.py
@@ -2,8 +2,8 @@
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 from openerp import _, api, fields, models
+from openerp.exceptions import ValidationError, UserError
 from openerp.addons import decimal_precision as dp
-from openerp.exceptions import UserError
 
 
 class RmaOrderLine(models.Model):
@@ -44,14 +44,18 @@ class RmaOrderLine(models.Model):
     invoice_address_id = fields.Many2one(
         'res.partner', string='Partner invoice address',
         default=_default_invoice_address,
-        help="Invoice address for current rma order.")
-
+        readonly=True, states={'draft': [('readonly', False)]},
+        help="Invoice address for current rma order.",
+    )
     refund_count = fields.Integer(
         compute=_compute_refund_count, string='# of Refunds', default=0)
-    invoice_line_id = fields.Many2one('account.invoice.line',
-                                      string='Invoice Line',
-                                      ondelete='restrict',
-                                      index=True)
+    invoice_line_id = fields.Many2one(
+        comodel_name='account.invoice.line',
+        string='Originating Invoice Line',
+        ondelete='restrict',
+        index=True,
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
     refund_line_ids = fields.One2many(comodel_name='account.invoice.line',
                                       inverse_name='rma_line_id',
                                       string='Refund Lines',
@@ -62,7 +66,9 @@ class RmaOrderLine(models.Model):
     refund_policy = fields.Selection([
         ('no', 'No refund'), ('ordered', 'Based on Ordered Quantities'),
         ('received', 'Based on Received Quantities')], string="Refund Policy",
-        required=True, default='no')
+        required=True, default='no',
+        readonly=True, states={'draft': [('readonly', False)]},
+    )
     qty_to_refund = fields.Float(
         string='Qty To Refund', copy=False,
         digits=dp.get_precision('Product Unit of Measure'), readonly=True,
@@ -72,20 +78,81 @@ class RmaOrderLine(models.Model):
         digits=dp.get_precision('Product Unit of Measure'),
         readonly=True, compute=_compute_qty_refunded, store=True)
 
+    @api.multi
+    def _prepare_rma_line_from_inv_line(self, line):
+        self.ensure_one()
+        if not self.type:
+            self.type = self._get_default_type()
+        if self.type == 'customer':
+            operation = line.product_id.rma_customer_operation_id or \
+                line.product_id.categ_id.rma_customer_operation_id
+        else:
+            operation = line.product_id.rma_supplier_operation_id or \
+                line.product_id.categ_id.rma_supplier_operation_id
+        if not operation:
+            operation = self.env['rma.operation'].search(
+                [('type', '=', self.type)], limit=1)
+            if not operation:
+                raise ValidationError("Please define an operation first")
+
+        if not operation.in_route_id or not operation.out_route_id:
+            route = self.env['stock.location.route'].search(
+                [('rma_selectable', '=', True)], limit=1)
+            if not route:
+                raise ValidationError("Please define an rma route")
+
+        if not operation.in_warehouse_id or not operation.out_warehouse_id:
+            warehouse = self.env['stock.warehouse'].search(
+                [('company_id', '=', self.company_id.id),
+                 ('lot_rma_id', '!=', False)], limit=1)
+            if not warehouse:
+                raise ValidationError("Please define a warehouse with a"
+                                      " default rma location")
+        data = {
+            'product_id': line.product_id.id,
+            'origin': line.invoice_id.number,
+            'uom_id': line.uom_id.id,
+            'operation_id': operation.id,
+            'product_qty': line.quantity,
+            'price_unit': line.invoice_id.currency_id.compute(
+                line.price_unit, line.currency_id, round=False),
+            'delivery_address_id': line.invoice_id.partner_id.id,
+            'invoice_address_id': line.invoice_id.partner_id.id,
+            'receipt_policy': operation.receipt_policy,
+            'refund_policy': operation.refund_policy,
+            'delivery_policy': operation.delivery_policy,
+            'in_warehouse_id': operation.in_warehouse_id.id or warehouse.id,
+            'out_warehouse_id': operation.out_warehouse_id.id or warehouse.id,
+            'in_route_id': operation.in_route_id.id or route.id,
+            'out_route_id': operation.out_route_id.id or route.id,
+            'location_id': (operation.location_id.id or
+                            operation.in_warehouse_id.lot_rma_id.id or
+                            warehouse.lot_rma_id.id),
+        }
+        return data
+
+    @api.onchange('invoice_line_id')
+    def _onchange_invoice_line_id(self):
+        if not self.invoice_line_id:
+            return
+        data = self._prepare_rma_line_from_inv_line(
+            self.invoice_line_id)
+        self.update(data)
+        self._remove_other_data_origin('invoice_line_id')
+
+    @api.multi
+    def _remove_other_data_origin(self, exception):
+        res = super(RmaOrderLine, self)._remove_other_data_origin(exception)
+        if not exception == 'invoice_line_id':
+            self.invoice_line_id = False
+        return res
+
     @api.onchange('operation_id')
     def _onchange_operation_id(self):
         result = super(RmaOrderLine, self)._onchange_operation_id()
         if not self.operation_id:
             return result
         self.refund_policy = self.operation_id.refund_policy
-        return result
-
-    @api.onchange('invoice_line_id')
-    def _onchange_invoice_line_id(self):
-        result = {}
-        if not self.invoice_line_id:
-            return result
-        self.origin = self.invoice_id.number
         return result
 
     @api.multi
