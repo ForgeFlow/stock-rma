7762be19f316cb0719b2a086b041feba5ac88f33
lreficent <lois.rilo@eficent.com>
2017-10-30 12:54:18 +0100

    pylint

--- rma_repair/models/mrp_repair.py
+++ rma_repair/models/mrp_repair.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from openerp import api, fields, models
+from openerp import fields, models
 
 
 class MrpRepair(models.Model):
