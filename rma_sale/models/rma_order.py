62adc3586d693250e9e534f6ffd08109a6f38d38
aheficent <ahenriquez@eficent.com>
2017-12-20 10:21:32 +0100

    [MIG]rma_sale to v10

--- rma_sale/models/rma_order.py
+++ rma_sale/models/rma_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class RmaOrder(models.Model):
