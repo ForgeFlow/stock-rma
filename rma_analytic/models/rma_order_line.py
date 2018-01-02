0bbe59219ec108ef16b71d8f60ef3fa25b20916d
aheficent <ahenriquez@eficent.com>
2018-01-02 14:32:11 +0100

    [ADD]rma_analytic

---
+++ rma_analytic/models/rma_order_line.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2018 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, fields, models
+
+
+class RmaOrderLine(models.Model):
+
+    _inherit = "rma.order.line"
+
+    analytic_account_id = fields.Many2one(
+        comodel_name='account.analytic',
+        string='Analytic Account',
+    )
