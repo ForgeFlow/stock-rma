b890a52ac09a27d3d6550f9fb6f08b96f298676c
aheficent <ahenriquez@eficent.com>
2018-01-03 14:59:06 +0100

    [ADD]rma_account_analytic

---
+++ rma_account_analytic/models/invoice.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2018 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import _, api, exceptions, models
+
+
+class AccountInvocieLine(models.Model):
+
+    _inherit = "account.invoice.line"
+
+
+@api.constrains('analytic_account_id')
+def check_analytic(self):
+    for inv in self:
+        if inv.analytic_account_id != inv.rma_line_id.analytic_account_id:
+            raise exceptions.ValidationError(
+                _("The analytic account in the invoice it's not the same"
+                  " as in the rma line"))
