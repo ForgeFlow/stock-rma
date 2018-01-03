de0231abffe0ed84cbfac0cbbfcea02fbcf8d5fd
aheficent <ahenriquez@eficent.com>
2018-01-03 15:20:45 +0100

    [ADD]rma_purchase_analytic [FIX]various

--- rma_analytic/__manifest__.py
+++ rma_analytic/__manifest__.py
@@ -5,7 +5,8 @@
 {
     "name": "Analytic Account in RMA",
     "version": "10.0.1.0.0",
-    "author": "Eficent",
+    "author": "Eficent,"
+              "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "http://www.eficent.com",
     "category": "Analytic",
