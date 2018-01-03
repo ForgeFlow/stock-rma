a39152808789244a344560c222a6fe6554a211dd
aheficent <ahenriquez@eficent.com>
2018-01-03 15:13:16 +0100

    [ADD]rma_sale_analytic

---
+++ rma_sale_analytic/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Analytic Account in RMA",
+    "version": "10.0.1.0.0",
+    "author": "Eficent",
+    "license": "LGPL-3",
+    "website": "http://www.eficent.com",
+    "category": "Analytic",
+    "depends": ["rma_account", "rma_analytic", "stock_analytic_account"],
+    "data": [
+    ],
+    'installable': True,
+}
