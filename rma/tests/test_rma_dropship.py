b60a0241d08cfa4fe27df859e27a3e719841b42a
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2018-01-05 16:43:54 +0530

    [MIG] Migrated UT & Fixed Travis

--- rma/tests/test_rma_dropship.py
+++ rma/tests/test_rma_dropship.py
@@ -9,6 +9,18 @@ class TestRmaDropship(test_rma.TestRma):
 
     def setUp(self):
         super(TestRmaDropship, self).setUp()
+        self.product_id.write(
+            {'rma_customer_operation_id': self.rma_cust_replace_op_id.id,
+             'rma_supplier_operation_id': self.rma_sup_replace_op_id.id})
+        self.product_1.write(
+            {'rma_customer_operation_id': self.rma_cust_replace_op_id.id,
+             'rma_supplier_operation_id': self.rma_sup_replace_op_id.id})
+        self.product_2.write(
+            {'rma_customer_operation_id': self.rma_cust_replace_op_id.id,
+             'rma_supplier_operation_id': self.rma_sup_replace_op_id.id})
+        self.product_3.write(
+            {'rma_customer_operation_id': self.rma_cust_replace_op_id.id,
+             'rma_supplier_operation_id': self.rma_sup_replace_op_id.id})
         products2move = [(self.product_1, 3), (self.product_2, 5),
                          (self.product_3, 2)]
         self.rma_droship_id = self._create_rma_from_move(
@@ -21,7 +33,10 @@ class TestRmaDropship(test_rma.TestRma):
             'active_ids': self.rma_droship_id.rma_line_ids.ids,
             'active_model': 'rma.order.line',
             'active_id': 1
-        }).create({})
+        }).create({'partner_id': self.partner_id.id,
+                   'supplier_rma_id': self.rma_droship_id.id,
+                   })
+
         res = wizard.make_supplier_rma()
         supplier_rma = self.rma.browse(res['res_id'])
         for line in supplier_rma.rma_line_ids:
@@ -55,26 +70,26 @@ class TestRmaDropship(test_rma.TestRma):
             if line.product_id == self.product_1:
                 self.assertEquals(line.qty_to_receive, 3,
                                   "Wrong qty to receive")
-                self.assertEquals(line.qty_to_deliver, 3,
-                                  "Wrong qty to deliver")
-                self.assertEquals(line.qty_incoming, 3,
+                self.assertEquals(line.qty_received, 0,
+                                  "Wrong qty receive")
+                self.assertEquals(line.qty_outgoing, 0,
                                   "Wrong qty outgoing")
             if line.product_id == self.product_2:
                 self.assertEquals(line.qty_to_receive, 5,
                                   "Wrong qty to receive")
-                self.assertEquals(line.qty_to_deliver, 5,
-                                  "Wrong qty to deliver")
-                self.assertEquals(line.qty_incoming, 5,
+                self.assertEquals(line.qty_delivered, 0,
+                                  "Wrong qty deliver")
+                self.assertEquals(line.qty_outgoing, 0,
                                   "Wrong qty outgoing")
             if line.product_id == self.product_3:
                 self.assertEquals(line.qty_to_receive, 2,
                                   "Wrong qty to receive")
-                self.assertEquals(line.qty_to_deliver, 2,
-                                  "Wrong qty to deliver")
-                self.assertEquals(line.qty_incoming, 2,
+                self.assertEquals(line.qty_delivered, 0,
+                                  "Wrong qty deliver")
+                self.assertEquals(line.qty_outgoing, 0,
                                   "Wrong qty outgoing")
 
-        for line in self.rma_droship_id.rma_line_ids:
+        for line in self.rma_droship_id.rma_line_ids[0]:
             if line.product_id == self.product_1:
                 self.assertEquals(line.qty_to_supplier_rma, 0,
                                   "Wrong qty to supplier rma")
