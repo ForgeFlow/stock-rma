78fdcccb976f657d09fc505508c15b2c448684ac
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2018-01-10 13:54:55 +0100

    [FIX] Fixed UT & Travis

--- rma_analytic/tests/test_rma_analytic.py
+++ rma_analytic/tests/test_rma_analytic.py
@@ -2,7 +2,7 @@
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from . import test_rma
+from odoo.addons.rma.tests import test_rma
 
 
 class TestRmaAnalytic(test_rma.TestRma):
@@ -11,20 +11,22 @@ class TestRmaAnalytic(test_rma.TestRma):
         super(TestRmaAnalytic, self).setUp()
         products2move = [(self.product_1, 3), (self.product_2, 5),
                          (self.product_3, 2)]
-        self.rma_id = self._create_rma_from_move(
+        self.rma_ana_id = self._create_rma_from_move(
             products2move, 'supplier', self.env.ref('base.res_partner_1'),
             dropship=False)
-        self.analytic_1 = self.env['account.analytic.account'].create({
+
+    def _prepare_move(self, product, qty, src, dest, picking_in):
+        res = super(TestRmaAnalytic, self)._prepare_move(
+            product, qty, src, dest, picking_in)
+        analytic_1 = self.env['account.analytic.account'].create({
             'name': 'Test account #1',
         })
-
-    def _prepare_move(self, product, qty, src, dest):
-        res = super(TestRmaAnalytic, self)._prepare_move(
-            product, qty, src, dest)
-        res.update(analytic_account_id=self.analytic_1.id)
+        res.update({'analytic_account_id': analytic_1.id})
         return res
 
     def test_analytic(self):
-        for line in self.rma_id.line_ids:
-            self.assertEqual(line.analytic_account_id, self.analytic_1,
-                             "the analytic account is not propagated")
+        for line in self.rma_ana_id.rma_line_ids:
+            for move in line.move_ids:
+                self.assertEqual(
+                    line.analytic_account_id, move.analytic_account_id,
+                    "the analytic account is not propagated")
