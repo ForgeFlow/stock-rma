d2ea82d0f0694a076af7b9c6228d49495958cdbc
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-02-09 22:55:14 +0530

    [11.0] MIG: rma_purchase

--- rma_purchase/models/rma_order.py
+++ rma_purchase/models/rma_order.py
@@ -1,18 +1,21 @@
-# -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
-from odoo import api, fields, models
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class RmaOrder(models.Model):
     _inherit = "rma.order"
 
-    @api.depends('rma_line_ids', 'rma_line_ids.procurement_ids')
     @api.multi
     def _compute_po_count(self):
         for rec in self:
-            purchases = rec.mapped('rma_line_ids.procurement_ids.purchase_id')
-            rec.po_count = len(purchases)
+            po_count = 0
+            for line in rec.rma_line_ids:
+                po_count += len(self.env['purchase.order'].search(
+                    [('origin', '=', line.name)]).ids)
+            rec.po_count = po_count
 
     @api.multi
     def _compute_origin_po_count(self):
@@ -30,7 +33,10 @@ class RmaOrder(models.Model):
     def action_view_purchase_order(self):
         action = self.env.ref('purchase.purchase_rfq')
         result = action.read()[0]
-        po_ids = self.mapped('rma_line_ids.procurement_ids.purchase_id').ids
+        po_ids = self.env['purchase.order'].search(
+            [('origin', '=', self.name)]).ids
+        if not po_ids:
+            raise ValidationError(_("No purchase order found!"))
         result['domain'] = [('id', 'in', po_ids)]
         return result
 
@@ -40,5 +46,7 @@ class RmaOrder(models.Model):
         result = action.read()[0]
         po_ids = self.mapped(
             'rma_line_ids.purchase_order_line_id.order_id').ids
+        if not po_ids:
+            raise ValidationError(_("No purchase order found!"))
         result['domain'] = [('id', 'in', po_ids)]
         return result
