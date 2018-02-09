4147bb62a468a2aa2637ab61ecf59fd9a6be5357
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-02-09 22:55:14 +0530

    [11.0] MIG: rma_purchase

--- rma_purchase/tests/__init__.py
+++ rma_purchase/tests/__init__.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
 from . import test_rma
 from . import test_supplier_rma
 from . import test_rma_dropship
