8132d8834fbfe76a43fd36dc87e6294f55492584
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-02-09 12:35:23 -0600

    [MIG] Migrate configuration and cleanup

--- rma/tests/__init__.py
+++ rma/tests/__init__.py
@@ -1,4 +1,4 @@
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright (C) 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 from . import test_rma
 from . import test_supplier_rma
