548e2b6e0a4ba6de2f38734d0d4e056db768b3cd
Lois Rilo <lois.rilo@eficent.com>
2018-05-14 14:58:42 +0200

    [9.0] rma:  * proper link between customer and supplier rma.  * fix wizard for generating supplier rma.  * several view issues.

--- rma/wizards/rma_make_picking.py
+++ rma/wizards/rma_make_picking.py
@@ -81,6 +81,8 @@ class RmaMakePicking(models.TransientModel):
     def _get_address(self, item):
         if item.line_id.customer_to_supplier:
             delivery_address = item.line_id.supplier_address_id
+        elif item.line_id.supplier_to_customer:
+            delivery_address = item.line_id.customer_address_id
         elif item.line_id.delivery_address_id:
             delivery_address = item.line_id.delivery_address_id
         elif item.line_id.partner_id:
