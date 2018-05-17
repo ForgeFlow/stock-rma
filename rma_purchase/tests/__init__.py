ca5dbf26a860b8bc72e9c574f5c0c6063fe123f2
Lois Rilo <lois.rilo@eficent.com>
2018-05-17 14:03:51 +0200

    [9.0][REW] rma_purchase: complete rework of tests

--- rma_purchase/tests/__init__.py
+++ rma_purchase/tests/__init__.py
@@ -1,6 +1,4 @@
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from . import test_rma
-from . import test_supplier_rma
-from . import test_rma_dropship
+from . import test_rma_purchase
