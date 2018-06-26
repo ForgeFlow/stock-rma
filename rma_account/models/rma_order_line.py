8e6dc6c242c78bf61a6f988de86ed36475e20d57
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-06-26 16:10:27 -0500

    [FIX] Add read access to invoice

--- rma_account/models/rma_order_line.py
+++ rma_account/models/rma_order_line.py
@@ -1,7 +1,7 @@
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, UserError
 from odoo.addons import decimal_precision as dp
 
