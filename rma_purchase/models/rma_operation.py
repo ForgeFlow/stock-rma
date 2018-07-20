321f83ba9ba4c1fac21ba1c1608a5b74bd6d7508
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-07-20 20:30:20 +0530

    [IMP] rma_operation

--- rma_purchase/models/rma_operation.py
+++ rma_purchase/models/rma_operation.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from openerp import api, fields, models, _
-from openerp.exceptions import ValidationError
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
 
 
 class RmaOperation(models.Model):
