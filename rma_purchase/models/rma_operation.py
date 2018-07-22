243cd170854c6e14fb4fc1842e2e34f2974301fc
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-07-22 13:15:55 +0530

    Revert "[IMP] rma_operation"
    This reverts commit 044bb2461acdb88e1ef98b1563e5ea7cb354918c.

--- rma_purchase/models/rma_operation.py
+++ rma_purchase/models/rma_operation.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from odoo import api, models, _
-from odoo.exceptions import ValidationError
+from openerp import api, fields, models, _
+from openerp.exceptions import ValidationError
 
 
 class RmaOperation(models.Model):
