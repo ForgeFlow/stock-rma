9525f29722099f9e7a0c35c26dc23ac97b8d08e5
aheficent <ahenriquez@eficent.com>
2018-08-22 13:36:20 +0200

    [MIG]rma_sale to v11

--- rma_sale/wizards/__init__.py
+++ rma_sale/wizards/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 from . import rma_order_line_make_sale_order
