1b3261a8c702768fe6515143df414e18c6aace13
aheficent <ahenriquez@eficent.com>
2018-08-22 13:36:20 +0200

    [MIG]rma_sale to v11

--- rma_sale/tests/test_rma_sale.py
+++ rma_sale/tests/test_rma_sale.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 from openerp.tests import common
