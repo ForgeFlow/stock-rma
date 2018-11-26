7f129d5073ba2a1c02ee94a9f6e6967c08d40b99
Adria Gil Sorribes <44768500+ageficent@users.noreply.github.com>
2018-11-26 09:09:42 +0100

    [11.0][MIG] rma_repair (#55)

--- rma_repair/models/mrp_repair.py
+++ rma_repair/models/mrp_repair.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class MrpRepair(models.Model):
