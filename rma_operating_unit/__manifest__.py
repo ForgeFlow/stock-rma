250656eb293bba0af8de99049d3b3948a196165d
Beñat Jimenez <benat.jimenez@eficent.com>
2019-02-14 09:51:31 +0100

    [MIG] rma_operating_unit: Migration to v12

--- rma_operating_unit/__manifest__.py
+++ rma_operating_unit/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# © 2017-19 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Operating Unit in RMA Groups",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent",
     "license": "LGPL-3",
-    "website": "http://www.eficent.com",
+    "website": "https://www.eficent.com",
     "category": "Operating Units",
     "depends": ["rma", "operating_unit"],
     "data": [
@@ -15,5 +14,4 @@
         "views/rma_order_view.xml",
         "views/rma_order_line_view.xml",
     ],
-    'installable': False,
 }
--- rma_operating_unit/models/rma_order_line.py
+++ rma_operating_unit/models/rma_order_line.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2018 Eficent Business and IT Consulting Services S.L.
+# © 2017-19 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, fields, models
