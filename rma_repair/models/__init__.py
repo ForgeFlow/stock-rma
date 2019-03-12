274e6b59c293bbff6b23f7ec16c50021858d97ee
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-03-12 17:21:19 +0100

    [MIG] rma_repair: Migration to 12.0

--- rma_repair/models/__init__.py
+++ rma_repair/models/__init__.py
@@ -1,6 +1,6 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from . import mrp_repair
+from . import repair
 from . import rma_order_line
 from . import rma_order
 from . import rma_operation
--- rma_repair/models/rma_order.py
+++ rma_repair/models/rma_order.py
@@ -11,14 +11,14 @@ class RmaOrder(models.Model):
     def _compute_repair_count(self):
         for rma in self:
             repairs = rma.mapped('rma_line_ids.repair_ids')
-            rma.rma_count = len(repairs)
+            rma.repair_count = len(repairs)
 
     repair_count = fields.Integer(
-        compute=_compute_repair_count, string='# of Repairs')
+        compute='_compute_repair_count', string='# of Repairs')
 
     @api.multi
     def action_view_repair_order(self):
-        action = self.env.ref('mrp_repair.action_repair_order_tree')
+        action = self.env.ref('repair.action_repair_order_tree')
         result = action.read()[0]
         repair_ids = self.mapped('rma_line_ids.repair_ids').ids
         result['domain'] = [('id', 'in', repair_ids)]
