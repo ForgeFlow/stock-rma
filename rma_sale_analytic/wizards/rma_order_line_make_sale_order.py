119c59339616ea77fbecd6dd3bfa586c5bbc4c48
Aaron Henriquez <ahenriquez@eficent.com>
2019-05-09 14:21:20 +0200

    [ADD]rma_sale_operating_unit [FIX]rma_sale_analytic

--- rma_sale_analytic/wizards/rma_order_line_make_sale_order.py
+++ rma_sale_analytic/wizards/rma_order_line_make_sale_order.py
@@ -9,9 +9,9 @@ class RmaLineMakeSaleOrder(models.TransientModel):
     _inherit = "rma.order.line.make.sale.order"
 
     @api.model
-    def _prepare_sale_order_line(self, so, item):
-        sale_line = super(
-            RmaLineMakeSaleOrder, self)._prepare_sale_order_line(so, item)
-        sale_line.update(
-            analytic_account_id=item.line_id.analytic_account_id.id)
-        return sale_line
+    def _prepare_sale_order(self, line):
+        res = super(
+            RmaLineMakeSaleOrder, self)._prepare_sale_order(line)
+        res.update(
+            project_id=line.analytic_account_id.id)
+        return res
