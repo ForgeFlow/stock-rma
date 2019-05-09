e24042c29d0b907195d91d9d978a4c6315d15d29
Aaron Henriquez <ahenriquez@eficent.com>
2019-05-09 14:21:57 +0200

    [ADD]rma_purchase_operating_unit [FIX]rma_purchase_analytic

--- rma_purchase_analytic/wizards/rma_add_purchase.py
+++ rma_purchase_analytic/wizards/rma_add_purchase.py
@@ -19,9 +19,8 @@ class RmaLineMakePurchaseOrder(models.TransientModel):
     _inherit = "rma.order.line.make.purchase.order"
     _description = "Make Purchases Order from RMA Line"
 
-
     @api.model
     def _prepare_purchase_order_line(self, po, item):
-        res = super(RmaLineMakePurchaseOrder, self)._prepare_purchase_order_line(po, line)
-        res['analytic_account_id'] = item.analytic_account_id.id
-        return vals
+        res = super(RmaLineMakePurchaseOrder, self)._prepare_purchase_order_line(po, item)
+        res['analytic_account_id'] = item.line_id.analytic_account_id.id
+        return res
