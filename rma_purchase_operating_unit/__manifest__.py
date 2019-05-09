4f5dea96ac1c5935f07f8fb47d46584e8095e92c
Aaron Henriquez <ahenriquez@eficent.com>
2019-05-09 14:21:57 +0200

    [ADD]rma_purchase_operating_unit [FIX]rma_purchase_analytic

---
+++ rma_purchase_operating_unit/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Analytic Account in RMA purchase",
+    "version": "10.0.1.0.0",
+    "author": "Eficent,"
+              "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "website": "http://www.eficent.com",
+    "category": "Analytic",
+    "depends": ["rma_operating_unit", "rma_purchase"],
+    "data": [
+    ],
+    'installable': True,
+}
