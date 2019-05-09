7e2dda155507d5c1edd1982f8125e14f2f56a351
Aaron Henriquez <ahenriquez@eficent.com>
2019-05-09 14:20:20 +0200

    [FIX]include routes in rma_sale

--- rma_sale/wizards/rma_order_line_make_sale_order.py
+++ rma_sale/wizards/rma_order_line_make_sale_order.py
@@ -60,7 +60,9 @@ class RmaLineMakeSaleOrder(models.TransientModel):
         return res
 
     @api.model
-    def _prepare_sale_order(self, out_warehouse, company):
+    def _prepare_sale_order(self, line):
+        out_warehouse = line.out_warehouse_id
+        company = line.company_id
         if not self.partner_id:
             raise exceptions.Warning(
                 _('Enter a customer.'))
@@ -80,6 +82,7 @@ class RmaLineMakeSaleOrder(models.TransientModel):
             'name': product.name,
             'order_id': so.id,
             'product_id': product.id,
+            'route_id': item.line_id.out_route_id.id,
             'product_uom': product.uom_po_id.id,
             'product_uom_qty': item.product_qty,
             'rma_line_id': item.line_id.id
@@ -104,8 +107,7 @@ class RmaLineMakeSaleOrder(models.TransientModel):
             if self.sale_order_id:
                 sale = self.sale_order_id
             if not sale:
-                po_data = self._prepare_sale_order(line.out_warehouse_id,
-                                                   line.company_id)
+                po_data = self._prepare_sale_order(line)
                 sale = sale_obj.create(po_data)
 
             so_line_data = self._prepare_sale_order_line(sale, item)
