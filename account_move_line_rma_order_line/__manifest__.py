ae7a8c8ef9790d4988663ddda33acc00ba53bbc0
ahenriquez <ahenriquez@eficent.com>
2019-08-05 16:36:41 +0200

    [MIG]account_move_line_rma_order_line to v12

--- account_move_line_rma_order_line/__manifest__.py
+++ account_move_line_rma_order_line/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Move Line Rma Order Line",
     "summary": "Introduces the rma order line to the journal items",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "http://www.github.com/OCA/account-financial-tools",
