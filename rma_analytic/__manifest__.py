1ad29d168af965463b6e5efadec06ab613a8f8a3
ahenriquez <ahenriquez@eficent.com>
2019-10-28 12:50:33 +0100

    [MIG]rma_analytic to v12

--- rma_analytic/__manifest__.py
+++ rma_analytic/__manifest__.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Analytic Account in RMA",
-    "version": "10.0.1.0.0",
-    "author": "Eficent,"
-              "Odoo Community Association (OCA)",
+    "version": "12.0.1.0.0",
+    "author": "Eficent," "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "http://www.eficent.com",
     "category": "Analytic",
-    "depends": ["rma", "analytic", "procurement_analytic",
-                'stock_analytic_account'],
-    "data": [
-        "views/rma_order_line_view.xml"
+    "depends": [
+        "rma_account",
+        "stock_analytic",
+        "procurement_mto_analytic",
     ],
-    'installable': False,
+    "data": ["views/rma_order_line_view.xml"],
+    "installable": True,
 }
