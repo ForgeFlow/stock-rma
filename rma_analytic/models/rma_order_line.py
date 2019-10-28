47abbf086da878c3c7b282dd65d59180e07e3838
ahenriquez <ahenriquez@eficent.com>
2019-10-28 12:50:33 +0100

    [MIG]rma_analytic to v12

--- rma_analytic/models/rma_order_line.py
+++ rma_analytic/models/rma_order_line.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-# © 2018 Eficent Business and IT Consulting Services S.L.
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class RmaOrderLine(models.Model):
@@ -10,6 +9,17 @@ class RmaOrderLine(models.Model):
     _inherit = "rma.order.line"
 
     analytic_account_id = fields.Many2one(
-        comodel_name='account.analytic.account',
-        string='Analytic Account',
+        comodel_name="account.analytic.account",
+        string="Analytic Account",
     )
+
+    @api.multi
+    def _prepare_rma_line_from_inv_line(self, line):
+        res = super(
+            RmaOrderLine, self
+        )._prepare_rma_line_from_inv_line(line)
+        if line.account_analytic_id:
+            res.update(
+                analytic_account_id=line.account_analytic_id.id
+            )
+        return res
---
+++ rma_analytic/wizards/rma_refund.py
@@ -0,0 +1,17 @@
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+from odoo import api, models
+
+
+class RmaRefund(models.TransientModel):
+    _inherit = "rma.refund"
+
+    @api.model
+    def prepare_refund_line(self, item, refund):
+        res = super(RmaRefund, self).prepare_refund_line(item, refund)
+        if item.line_id.analytic_account_id:
+            res.update(
+                account_analytic_id=item.line_id.analytic_account_id.id
+            )
+        return res
