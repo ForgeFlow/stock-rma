9ce5b53ee26700492693d5a249602233f1fc3705
ahenriquez <ahenriquez@eficent.com>
2019-10-29 16:53:47 +0100

    [FIX]UsreError to Validation Error

--- rma/models/rma_order.py
+++ rma/models/rma_order.py
@@ -1,8 +1,7 @@
 # Copyright (C) 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
+from odoo import api, fields, models
 from datetime import datetime
 
 
