245064141e78bf42d0b5c9d1f4e5443bfe78c0bf
mreficent <miquel.raich@eficent.com>
2019-10-30 19:38:23 +0100

    [FIX] tests

--- rma_sale/tests/test_rma_sale.py
+++ rma_sale/tests/test_rma_sale.py
@@ -1,7 +1,7 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestRmaSale(common.SingleTransactionCase):
