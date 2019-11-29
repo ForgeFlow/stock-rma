338dc3461cce3700705567963134552a8d1c9ed4
mreficent <miquel.raich@eficent.com>
2019-11-29 18:30:43 +0100

    [FIX] default_gets: avoid using shadowname 'fields'

--- rma_sale/wizards/rma_order_line_make_sale_order.py
+++ rma_sale/wizards/rma_order_line_make_sale_order.py
@@ -32,9 +32,9 @@ class RmaLineMakeSaleOrder(models.TransientModel):
         }
 
     @api.model
-    def default_get(self, fields):
+    def default_get(self, fields_list):
         res = super(RmaLineMakeSaleOrder, self).default_get(
-            fields)
+            fields_list)
         rma_line_obj = self.env['rma.order.line']
         rma_line_ids = self.env.context['active_ids'] or []
         active_model = self.env.context['active_model']
