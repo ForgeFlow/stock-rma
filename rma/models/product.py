c4c1a4ea1ae9514d5f085b539ec8f65442e1dde3
ahenriquez <ahenriquez@eficent.com>
2020-01-14 16:36:03 +0100

    [IMP] : black, isort

--- rma/models/product.py
+++ rma/models/product.py
@@ -5,11 +5,14 @@ from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     rma_customer_operation_id = fields.Many2one(
-        comodel_name="rma.operation", string="Default RMA Customer Operation")
+        comodel_name="rma.operation", string="Default RMA Customer Operation"
+    )
     rma_supplier_operation_id = fields.Many2one(
-        comodel_name="rma.operation", string="Default RMA Supplier Operation")
+        comodel_name="rma.operation", string="Default RMA Supplier Operation"
+    )
     rma_approval_policy = fields.Selection(
-        related="categ_id.rma_approval_policy", readonly=True)
+        related="categ_id.rma_approval_policy", readonly=True
+    )
