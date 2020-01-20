850c5ddc827981a544264511e518a0799ec36690
hveficent <hector.villarreal@forgeflow.com>
2020-01-20 08:10:19 +0100

    RMA as an APP

--- rma/models/res_config_settings.py
+++ rma/models/res_config_settings.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2017 ForgeFlow
+# Copyright (C) 2017-20 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 from odoo import fields, models
 
 
-class StockConfigSettings(models.TransientModel):
+class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     group_rma_delivery_address = fields.Boolean(
--- rma/wizards/__init__.py
+++ rma/wizards/__init__.py
@@ -1,7 +1,6 @@
-# Copyright (C) 2017 ForgeFlow
+# Copyright (C) 2017-20 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 from . import rma_add_stock_move
 from . import rma_make_picking
-from . import stock_config_settings
 from . import rma_order_line_make_supplier_rma
