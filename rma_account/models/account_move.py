d0e9befa14d50542d53a4592d5a076b4a68732d9
ahenriquez <ahenriquez@eficent.com>
2020-01-22 16:03:16 +0100

    [MIG]rma_account to v13

--- rma_account/models/account_move.py
+++ rma_account/models/account_move.py
@@ -5,13 +5,13 @@ from odoo import api, fields, models
 from odoo.tools.float_utils import float_compare
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
-    @api.depends("invoice_line_ids.rma_line_ids")
+    @api.depends("line_ids.rma_line_ids")
     def _compute_rma_count(self):
         for inv in self:
-            rmas = self.mapped("invoice_line_ids.rma_line_ids")
+            rmas = self.mapped("line_ids.rma_line_ids")
             inv.rma_count = len(rmas)
 
     def _prepare_invoice_line_from_rma_line(self, line):
@@ -19,18 +19,17 @@ class AccountInvoice(models.Model):
         if float_compare(qty, 0.0, precision_rounding=line.uom_id.rounding) <= 0:
             qty = 0.0
         # Todo fill taxes from somewhere
-        invoice_line = self.env["account.invoice.line"]
+        invoice_line = self.env["account.move.line"]
         data = {
             "purchase_line_id": line.id,
             "name": line.name + ": " + line.name,
-            "origin": line.origin,
-            "uom_id": line.uom_id.id,
+            "product_uom_id": line.uom_id.id,
             "product_id": line.product_id.id,
             "account_id": invoice_line.with_context(
                 {"journal_id": self.journal_id.id, "type": "in_invoice"}
             )._default_account(),
             "price_unit": line.company_id.currency_id.with_context(
-                date=self.date_invoice
+                date=self.date
             ).compute(line.price_unit, self.currency_id, round=False),
             "quantity": qty,
             "discount": 0.0,
@@ -45,12 +44,12 @@ class AccountInvoice(models.Model):
         if not self.partner_id:
             self.partner_id = self.add_rma_line_id.partner_id.id
 
-        new_line = self.env["account.invoice.line"]
-        if self.add_rma_line_id not in (self.invoice_line_ids.mapped("rma_line_id")):
+        new_line = self.env["account.move.line"]
+        if self.add_rma_line_id not in (self.line_ids.mapped("rma_line_id")):
             data = self._prepare_invoice_line_from_rma_line(self.add_rma_line_id)
             new_line = new_line.new(data)
             new_line._set_additional_fields(self)
-        self.invoice_line_ids += new_line
+        self.line_ids += new_line
         self.add_rma_line_id = False
         return {}
 
@@ -63,11 +62,10 @@ class AccountInvoice(models.Model):
         help="Create a refund in based on an existing rma_line",
     )
 
-    @api.multi
     def action_view_rma_supplier(self):
         action = self.env.ref("rma.action_rma_supplier_lines")
         result = action.read()[0]
-        rma_ids = self.mapped("invoice_line_ids.rma_line_ids").ids
+        rma_ids = self.mapped("line_ids.rma_line_ids").ids
         if rma_ids:
             # choose the view_mode accordingly
             if len(rma_ids) > 1:
@@ -78,11 +76,10 @@ class AccountInvoice(models.Model):
                 result["res_id"] = rma_ids[0]
         return result
 
-    @api.multi
     def action_view_rma_customer(self):
         action = self.env.ref("rma.action_rma_customer_lines")
         result = action.read()[0]
-        rma_ids = self.mapped("invoice_line_ids.rma_line_ids").ids
+        rma_ids = self.mapped("line_ids.rma_line_ids").ids
         if rma_ids:
             # choose the view_mode accordingly
             if len(rma_ids) > 1:
@@ -94,8 +91,8 @@ class AccountInvoice(models.Model):
         return result
 
 
-class AccountInvoiceLine(models.Model):
-    _inherit = "account.invoice.line"
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
 
     @api.model
     def name_search(self, name, args=None, operator="ilike", limit=100):
@@ -104,7 +101,7 @@ class AccountInvoiceLine(models.Model):
         will make impossible to get the desired result."""
         if not args:
             args = []
-        lines = self.search([("invoice_id.number", operator, name)] + args, limit=limit)
+        lines = self.search([("move_id.name", operator, name)] + args, limit=limit)
         res = lines.name_get()
         if limit:
             limit_rest = limit - len(lines)
@@ -113,50 +110,48 @@ class AccountInvoiceLine(models.Model):
             limit_rest = limit
         if limit_rest or not limit:
             args += [("id", "not in", lines.ids)]
-            res += super(AccountInvoiceLine, self).name_search(
+            res += super(AccountMoveLine, self).name_search(
                 name, args=args, operator=operator, limit=limit_rest
             )
         return res
 
-    @api.multi
     def name_get(self):
         res = []
         if self.env.context.get("rma"):
             for inv in self:
-                if inv.invoice_id.reference:
+                if inv.move_id.ref:
                     res.append(
                         (
                             inv.id,
                             "INV:%s | REF:%s | ORIG:%s | PART:%s | QTY:%s"
                             % (
-                                inv.invoice_id.number or "",
-                                inv.origin or "",
-                                inv.invoice_id.reference or "",
+                                inv.move_id.name or "",
+                                inv.move_id.invoice_origin or "",
+                                inv.move_id.ref or "",
                                 inv.product_id.name,
                                 inv.quantity,
                             ),
                         )
                     )
-                elif inv.invoice_id.number:
+                elif inv.move_id.name:
                     res.append(
                         (
                             inv.id,
                             "INV:%s | ORIG:%s | PART:%s | QTY:%s"
                             % (
-                                inv.invoice_id.number or "",
-                                inv.origin or "",
+                                inv.move_id.name or "",
+                                inv.move_id.invoice_origin or "",
                                 inv.product_id.name,
                                 inv.quantity,
                             ),
                         )
                     )
                 else:
-                    res.append(super(AccountInvoiceLine, inv).name_get()[0])
+                    res.append(super(AccountMoveLine, inv).name_get()[0])
             return res
         else:
-            return super(AccountInvoiceLine, self).name_get()
+            return super(AccountMoveLine, self).name_get()
 
-    @api.multi
     def _compute_rma_count(self):
         for invl in self:
             rma_lines = invl.mapped("rma_line_ids")
@@ -165,7 +160,7 @@ class AccountInvoiceLine(models.Model):
     rma_line_count = fields.Integer(compute="_compute_rma_count", string="# of RMA")
     rma_line_ids = fields.One2many(
         comodel_name="rma.order.line",
-        inverse_name="invoice_line_id",
+        inverse_name="account_move_line_id",
         string="RMA",
         readonly=True,
         help="This will contain the RMA lines for the invoice line",
