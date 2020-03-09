08308097cbafbbe89515b1b1850e0a3eca35bc5d
ahenriquez <ahenriquez@eficent.com>
2020-03-09 11:16:16 +0100

    [IMP] : black, isort

--- rma_sale/wizards/rma_add_sale.py
+++ rma_sale/wizards/rma_add_sale.py
@@ -6,90 +6,105 @@ from odoo.exceptions import ValidationError
 
 
 class RmaAddSale(models.TransientModel):
-    _name = 'rma_add_sale'
-    _description = 'Wizard to add rma lines from SO lines'
+    _name = "rma_add_sale"
+    _description = "Wizard to add rma lines from SO lines"
 
     @api.model
     def default_get(self, fields_list):
         res = super(RmaAddSale, self).default_get(fields_list)
-        rma_obj = self.env['rma.order']
-        rma_id = self.env.context['active_ids'] or []
-        active_model = self.env.context['active_model']
+        rma_obj = self.env["rma.order"]
+        rma_id = self.env.context["active_ids"] or []
+        active_model = self.env.context["active_model"]
         if not rma_id:
             return res
-        assert active_model == 'rma.order', 'Bad context propagation'
+        assert active_model == "rma.order", "Bad context propagation"
 
         rma = rma_obj.browse(rma_id)
-        res['rma_id'] = rma.id
-        res['partner_id'] = rma.partner_id.id
-        res['sale_id'] = False
-        res['sale_line_ids'] = False
+        res["rma_id"] = rma.id
+        res["partner_id"] = rma.partner_id.id
+        res["sale_id"] = False
+        res["sale_line_ids"] = False
         return res
 
     rma_id = fields.Many2one(
-        comodel_name='rma.order', string='RMA Order', readonly=True)
-    partner_id = fields.Many2one(comodel_name='res.partner', string='Partner',
-                                 readonly=True)
-    sale_id = fields.Many2one(comodel_name='sale.order', string='Order')
-    sale_line_ids = fields.Many2many('sale.order.line',
-                                     'rma_add_sale_add_line_rel',
-                                     'sale_line_id', 'rma_add_sale_id',
-                                     readonly=False,
-                                     string='Sale Lines')
+        comodel_name="rma.order", string="RMA Order", readonly=True
+    )
+    partner_id = fields.Many2one(
+        comodel_name="res.partner", string="Partner", readonly=True
+    )
+    sale_id = fields.Many2one(comodel_name="sale.order", string="Order")
+    sale_line_ids = fields.Many2many(
+        "sale.order.line",
+        "rma_add_sale_add_line_rel",
+        "sale_line_id",
+        "rma_add_sale_id",
+        readonly=False,
+        string="Sale Lines",
+    )
 
     def _prepare_rma_line_from_sale_order_line(self, line):
         operation = line.product_id.rma_customer_operation_id
         if not operation:
             operation = line.product_id.categ_id.rma_customer_operation_id
         if not operation:
-            operation = self.env['rma.operation'].search(
-                [('type', '=', self.rma_id.type)], limit=1)
+            operation = self.env["rma.operation"].search(
+                [("type", "=", self.rma_id.type)], limit=1
+            )
             if not operation:
                 raise ValidationError(_("Please define an operation first"))
         if not operation.in_route_id or not operation.out_route_id:
-            route = self.env['stock.location.route'].search(
-                [('rma_selectable', '=', True)], limit=1)
+            route = self.env["stock.location.route"].search(
+                [("rma_selectable", "=", True)], limit=1
+            )
             if not route:
                 raise ValidationError(_("Please define an rma route"))
         if not operation.in_warehouse_id or not operation.out_warehouse_id:
-            warehouse = self.env['stock.warehouse'].search(
-                [('company_id', '=', self.rma_id.company_id.id),
-                 ('lot_rma_id', '!=', False)], limit=1)
+            warehouse = self.env["stock.warehouse"].search(
+                [
+                    ("company_id", "=", self.rma_id.company_id.id),
+                    ("lot_rma_id", "!=", False),
+                ],
+                limit=1,
+            )
             if not warehouse:
-                raise ValidationError(_("Please define a warehouse with a "
-                                        "default rma location."))
+                raise ValidationError(
+                    _("Please define a warehouse with a " "default rma location.")
+                )
         data = {
-            'partner_id': self.partner_id.id,
-            'sale_line_id': line.id,
-            'product_id': line.product_id.id,
-            'origin': line.order_id.name,
-            'uom_id': line.product_uom.id,
-            'operation_id': operation.id,
-            'product_qty': line.product_uom_qty,
-            'delivery_address_id': self.sale_id.partner_id.id,
-            'invoice_address_id': self.sale_id.partner_id.id,
-            'price_unit': line.currency_id.compute(
-                line.price_unit, line.currency_id, round=False),
-            'rma_id': self.rma_id.id,
-            'in_route_id': operation.in_route_id.id or route.id,
-            'out_route_id': operation.out_route_id.id or route.id,
-            'receipt_policy': operation.receipt_policy,
-            'location_id': (operation.location_id.id or
-                            operation.in_warehouse_id.lot_rma_id.id or
-                            warehouse.lot_rma_id.id),
-            'refund_policy': operation.refund_policy,
-            'delivery_policy': operation.delivery_policy,
-            'in_warehouse_id': operation.in_warehouse_id.id or warehouse.id,
-            'out_warehouse_id': operation.out_warehouse_id.id or warehouse.id,
+            "partner_id": self.partner_id.id,
+            "sale_line_id": line.id,
+            "product_id": line.product_id.id,
+            "origin": line.order_id.name,
+            "uom_id": line.product_uom.id,
+            "operation_id": operation.id,
+            "product_qty": line.product_uom_qty,
+            "delivery_address_id": self.sale_id.partner_id.id,
+            "invoice_address_id": self.sale_id.partner_id.id,
+            "price_unit": line.currency_id.compute(
+                line.price_unit, line.currency_id, round=False
+            ),
+            "rma_id": self.rma_id.id,
+            "in_route_id": operation.in_route_id.id or route.id,
+            "out_route_id": operation.out_route_id.id or route.id,
+            "receipt_policy": operation.receipt_policy,
+            "location_id": (
+                operation.location_id.id
+                or operation.in_warehouse_id.lot_rma_id.id
+                or warehouse.lot_rma_id.id
+            ),
+            "refund_policy": operation.refund_policy,
+            "delivery_policy": operation.delivery_policy,
+            "in_warehouse_id": operation.in_warehouse_id.id or warehouse.id,
+            "out_warehouse_id": operation.out_warehouse_id.id or warehouse.id,
         }
         return data
 
     @api.model
     def _get_rma_data(self):
         data = {
-            'date_rma': fields.Datetime.now(),
-            'delivery_address_id': self.sale_id.partner_id.id,
-            'invoice_address_id': self.sale_id.partner_id.id
+            "date_rma": fields.Datetime.now(),
+            "delivery_address_id": self.sale_id.partner_id.id,
+            "invoice_address_id": self.sale_id.partner_id.id,
         }
         return data
 
@@ -102,7 +117,7 @@ class RmaAddSale(models.TransientModel):
 
     @api.multi
     def add_lines(self):
-        rma_line_obj = self.env['rma.order.line']
+        rma_line_obj = self.env["rma.order.line"]
         existing_sale_lines = self._get_existing_sale_lines()
         for line in self.sale_line_ids:
             # Load a PO line only once
@@ -112,4 +127,4 @@ class RmaAddSale(models.TransientModel):
         rma = self.rma_id
         data_rma = self._get_rma_data()
         rma.write(data_rma)
-        return {'type': 'ir.actions.act_window_close'}
+        return {"type": "ir.actions.act_window_close"}
--- rma_sale/wizards/rma_order_line_make_sale_order.py
+++ rma_sale/wizards/rma_order_line_make_sale_order.py
@@ -1,8 +1,9 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-import odoo.addons.decimal_precision as dp
 from odoo import _, api, exceptions, fields, models
+
+import odoo.addons.decimal_precision as dp
 
 
 class RmaLineMakeSaleOrder(models.TransientModel):
@@ -10,111 +11,118 @@ class RmaLineMakeSaleOrder(models.TransientModel):
     _description = "Make Sales Order from RMA Line"
 
     partner_id = fields.Many2one(
-        comodel_name='res.partner', string='Customer', required=False,
-        domain=[('customer', '=', True)])
+        comodel_name="res.partner",
+        string="Customer",
+        required=False,
+        domain=[("customer", "=", True)],
+    )
     item_ids = fields.One2many(
-        comodel_name='rma.order.line.make.sale.order.item',
-        inverse_name='wiz_id', string='Items')
+        comodel_name="rma.order.line.make.sale.order.item",
+        inverse_name="wiz_id",
+        string="Items",
+    )
     sale_order_id = fields.Many2one(
-        comodel_name='sale.order', string='Sales Order', required=False,
-        domain=[('state', '=', 'draft')])
+        comodel_name="sale.order",
+        string="Sales Order",
+        required=False,
+        domain=[("state", "=", "draft")],
+    )
 
     @api.model
     def _prepare_item(self, line):
         return {
-            'line_id': line.id,
-            'product_id': line.product_id.id,
-            'name': line.product_id.name,
-            'product_qty': line.qty_to_sell,
-            'rma_id': line.rma_id.id,
-            'out_warehouse_id': line.out_warehouse_id.id,
-            'product_uom_id': line.uom_id.id,
+            "line_id": line.id,
+            "product_id": line.product_id.id,
+            "name": line.product_id.name,
+            "product_qty": line.qty_to_sell,
+            "rma_id": line.rma_id.id,
+            "out_warehouse_id": line.out_warehouse_id.id,
+            "product_uom_id": line.uom_id.id,
         }
 
     @api.model
     def default_get(self, fields_list):
-        res = super(RmaLineMakeSaleOrder, self).default_get(
-            fields_list)
-        rma_line_obj = self.env['rma.order.line']
-        rma_line_ids = self.env.context['active_ids'] or []
-        active_model = self.env.context['active_model']
+        res = super(RmaLineMakeSaleOrder, self).default_get(fields_list)
+        rma_line_obj = self.env["rma.order.line"]
+        rma_line_ids = self.env.context["active_ids"] or []
+        active_model = self.env.context["active_model"]
 
         if not rma_line_ids:
             return res
-        assert active_model == 'rma.order.line', 'Bad context propagation'
+        assert active_model == "rma.order.line", "Bad context propagation"
 
         items = []
         lines = rma_line_obj.browse(rma_line_ids)
         for line in lines:
             items.append([0, 0, self._prepare_item(line)])
-        customers = lines.mapped('partner_id')
+        customers = lines.mapped("partner_id")
         if len(customers) == 1:
-            res['partner_id'] = customers.id
+            res["partner_id"] = customers.id
         else:
             raise exceptions.Warning(
-                _('Only RMA lines from the same partner can be processed at '
-                  'the same time'))
-        res['item_ids'] = items
+                _(
+                    "Only RMA lines from the same partner can be processed at "
+                    "the same time"
+                )
+            )
+        res["item_ids"] = items
         return res
 
     @api.model
     def _prepare_sale_order(self, out_warehouse, company, item):
         if not self.partner_id:
-            raise exceptions.Warning(
-                _('Enter a customer.'))
+            raise exceptions.Warning(_("Enter a customer."))
         customer = self.partner_id
         data = {
-            'origin': item.line_id.name,
-            'partner_id': customer.id,
-            'warehouse_id': out_warehouse.id,
-            'company_id': company.id,
-            }
+            "origin": item.line_id.name,
+            "partner_id": customer.id,
+            "warehouse_id": out_warehouse.id,
+            "company_id": company.id,
+        }
         return data
 
     @api.model
     def _prepare_sale_order_line(self, so, item):
         product = item.product_id
         vals = {
-            'name': product.name,
-            'order_id': so.id,
-            'product_id': product.id,
-            'product_uom': product.uom_po_id.id,
-            'product_uom_qty': item.product_qty,
-            'rma_line_id': item.line_id.id
+            "name": product.name,
+            "order_id": so.id,
+            "product_id": product.id,
+            "product_uom": product.uom_po_id.id,
+            "product_uom_qty": item.product_qty,
+            "rma_line_id": item.line_id.id,
         }
         if item.free_of_charge:
-            vals['price_unit'] = 0.0
+            vals["price_unit"] = 0.0
         return vals
 
     @api.multi
     def make_sale_order(self):
         res = []
-        sale_obj = self.env['sale.order']
-        so_line_obj = self.env['sale.order.line']
+        sale_obj = self.env["sale.order"]
+        so_line_obj = self.env["sale.order.line"]
         sale = False
 
         for item in self.item_ids:
             line = item.line_id
             if item.product_qty <= 0.0:
-                raise exceptions.Warning(
-                    _('Enter a positive quantity.'))
+                raise exceptions.Warning(_("Enter a positive quantity."))
 
             if self.sale_order_id:
                 sale = self.sale_order_id
             if not sale:
                 po_data = self._prepare_sale_order(
-                    line.out_warehouse_id,
-                    line.company_id,
-                    item)
+                    line.out_warehouse_id, line.company_id, item
+                )
                 sale = sale_obj.create(po_data)
 
             so_line_data = self._prepare_sale_order_line(sale, item)
             so_line_obj.create(so_line_data)
             res.append(sale.id)
 
-        action = self.env.ref('sale.action_orders')
+        action = self.env.ref("sale.action_orders")
         result = action.read()[0]
-        result['domain'] = "[('id','in', ["+','.join(map(str, res))+"])]"
+        result["domain"] = "[('id','in', [" + ",".join(map(str, res)) + "])]"
         return result
 
 
@@ -123,18 +131,19 @@ class RmaLineMakeSaleOrderItem(models.TransientModel):
     _description = "RMA Line Make Sale Order Item"
 
     wiz_id = fields.Many2one(
-        comodel_name='rma.order.line.make.sale.order', string='Wizard')
-    line_id = fields.Many2one(
-        comodel_name='rma.order.line', string='RMA Line')
+        comodel_name="rma.order.line.make.sale.order", string="Wizard"
+    )
+    line_id = fields.Many2one(comodel_name="rma.order.line", string="RMA Line")
     rma_id = fields.Many2one(
-        comodel_name='rma.order', related='line_id.rma_id', readonly=False)
-    product_id = fields.Many2one(
-        comodel_name='product.product', string='Product')
-    name = fields.Char(string='Description')
+        comodel_name="rma.order", related="line_id.rma_id", readonly=False
+    )
+    product_id = fields.Many2one(comodel_name="product.product", string="Product")
+    name = fields.Char(string="Description")
     product_qty = fields.Float(
-        string='Quantity to sell', digits=dp.get_precision('Product UoS'))
-    product_uom_id = fields.Many2one(
-        comodel_name='uom.uom', string='UoM')
+        string="Quantity to sell", digits=dp.get_precision("Product UoS")
+    )
+    product_uom_id = fields.Many2one(comodel_name="uom.uom", string="UoM")
     out_warehouse_id = fields.Many2one(
-        comodel_name='stock.warehouse', string='Outbound Warehouse')
-    free_of_charge = fields.Boolean(string='Free of Charge')
+        comodel_name="stock.warehouse", string="Outbound Warehouse"
+    )
+    free_of_charge = fields.Boolean(string="Free of Charge")
