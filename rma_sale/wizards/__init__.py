a9059078fdfca21e770a56dafd60c48d554f5014
ahenriquez <ahenriquez@eficent.com>
2020-03-09 11:23:53 +0100

    [MIG]rma_sale v13

--- rma_sale/wizards/__init__.py
+++ rma_sale/wizards/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2020 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 from . import rma_order_line_make_sale_order
