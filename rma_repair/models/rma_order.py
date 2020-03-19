413b49dcf17bca1f3e390c1f21eefea783dcd055
ahenriquez <ahenriquez@eficent.com>
2020-03-19 15:38:40 +0100

    [IMP] : black, isort

--- rma_repair/models/rma_order.py
+++ rma_repair/models/rma_order.py
@@ -10,16 +10,17 @@ class RmaOrder(models.Model):
     @api.multi
     def _compute_repair_count(self):
         for rma in self:
-            repairs = rma.mapped('rma_line_ids.repair_ids')
+            repairs = rma.mapped("rma_line_ids.repair_ids")
             rma.repair_count = len(repairs)
 
     repair_count = fields.Integer(
-        compute='_compute_repair_count', string='# of Repairs')
+        compute="_compute_repair_count", string="# of Repairs"
+    )
 
     @api.multi
     def action_view_repair_order(self):
-        action = self.env.ref('repair.action_repair_order_tree')
+        action = self.env.ref("repair.action_repair_order_tree")
         result = action.read()[0]
-        repair_ids = self.mapped('rma_line_ids.repair_ids').ids
-        result['domain'] = [('id', 'in', repair_ids)]
+        repair_ids = self.mapped("rma_line_ids.repair_ids").ids
+        result["domain"] = [("id", "in", repair_ids)]
         return result
