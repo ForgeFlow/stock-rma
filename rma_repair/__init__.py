723212b57ebda7b9fa2eb35f6f1014b6c4ea5348
ahenriquez <ahenriquez@eficent.com>
2020-03-19 15:43:10 +0100

    [MIG]rma_repair to v13

--- rma_repair/__init__.py
+++ rma_repair/__init__.py
@@ -1,4 +1,5 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+# Copyright 2020 ForgeFlow S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import models
 from . import wizards
--- rma_repair/wizards/__init__.py
+++ rma_repair/wizards/__init__.py
@@ -1,3 +1,4 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+# Copyright 2020 ForgeFlow S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import rma_order_line_make_repair
