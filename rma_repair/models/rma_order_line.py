7e2604709c36ed4b36e14bb9db60b3157d97a319
Mateu Griful <mateu.griful@forgeflow.com>
2021-01-27 13:24:14 +0100

    [MIG] rma_repair: Migration to 14.0

--- rma_repair/models/rma_order_line.py
+++ rma_repair/models/rma_order_line.py
@@ -84,7 +84,8 @@ class RmaOrderLine(models.Model):
     )
 
     delivery_policy = fields.Selection(
-        selection_add=[("repair", "Based on Repair Quantities")]
+        selection_add=[("repair", "Based on Repair Quantities")],
+        ondelete={"repair": lambda recs: recs.write({"delivery_policy": "no"})},
     )
     qty_to_pay = fields.Float(compute="_compute_qty_to_pay")
     qty_to_deliver = fields.Float(compute="_compute_qty_to_deliver")
@@ -100,13 +101,17 @@ class RmaOrderLine(models.Model):
         "repair_ids.invoice_status",
     )
     def _compute_qty_to_pay(self):
-        for rec in self.filtered(lambda l: l.delivery_policy == "repair"):
-            qty_to_pay = 0.0
-            for repair in rec.repair_ids.filtered(
-                lambda r: r.invoice_method != "none" and r.invoice_status != "posted"
-            ):
-                qty_to_pay += repair.product_qty
-            rec.qty_to_pay = qty_to_pay
+        for rec in self:
+            if rec.delivery_policy == "repair":
+                qty_to_pay = 0.0
+                for repair in rec.repair_ids.filtered(
+                    lambda r: r.invoice_method != "none"
+                    and r.invoice_status != "posted"
+                ):
+                    qty_to_pay += repair.product_qty
+                rec.qty_to_pay = qty_to_pay
+            else:
+                rec.qty_to_pay = 0.0
 
     def action_view_repair_order(self):
         action = self.env.ref("repair.action_repair_order_tree")
