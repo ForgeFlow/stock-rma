83327962d8dd7fc77d7be28c12d436aa70dc978c
Lois Rilo <lois.rilo@forgeflow.com>
2021-03-25 18:13:18 +0100

    [14.0][FIX] rma_repair: * fix quantity computation * use a payment_state field instead of inovice state one. * Use proper digits and unit conversion. * Add a filter "To repair". * Remove dead code and clean warnings.

--- rma_repair/models/repair.py
+++ rma_repair/models/repair.py
@@ -1,4 +1,4 @@
-# Copyright 2020 ForgeFlow S.L.
+# Copyright 2020-21 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -13,6 +13,6 @@ class RepairOrder(models.Model):
     under_warranty = fields.Boolean(
         related="rma_line_id.under_warranty", readonly=False
     )
-    invoice_status = fields.Selection(
-        related="invoice_id.state", string="Invoice Status"
+    payment_state = fields.Selection(
+        related="invoice_id.payment_state", string="Payment Status"
     )
