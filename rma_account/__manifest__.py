882d5c1c9205f36e7281406738d78f686663c72f
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-01-07 14:28:00 +0530

    [MIG] rma_account: Migration to 15.0

--- rma_account/__manifest__.py
+++ rma_account/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "RMA Account",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "LGPL-3",
     "category": "RMA",
     "summary": "Integrates RMA with Invoice Processing",
