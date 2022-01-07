af0f6e9106ebef6dd136cf8c0f943c47772d5c34
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-01-07 14:28:00 +0530

    [MIG] rma_account: Migration to 15.0

--- rma_account/tests/test_rma_account.py
+++ rma_account/tests/test_rma_account.py
@@ -220,7 +220,7 @@ class TestRmaAccount(common.SingleTransactionCase):
             {"customer": True, "active_ids": rma.ids, "active_model": "rma.order.line"}
         ).create({"description": "Test refund"})
         make_refund.invoice_refund()
-        rma.refund_line_ids.move_id.post()
+        rma.refund_line_ids.move_id.action_post()
         rma._compute_refund_count()
         self.assertEqual(rma.refund_count, 1)
         self.assertEqual(rma.qty_to_refund, 0.0)
@@ -258,7 +258,7 @@ class TestRmaAccount(common.SingleTransactionCase):
             {"customer": True, "active_ids": rma.ids, "active_model": "rma.order.line"}
         ).create({"description": "Test refund"})
         make_refund.invoice_refund()
-        rma.refund_line_ids.move_id.post()
+        rma.refund_line_ids.move_id.action_post()
         rma._compute_refund_count()
         self.assertEqual(
             self.operation_1.refund_journal_id, rma.refund_line_ids.journal_id
