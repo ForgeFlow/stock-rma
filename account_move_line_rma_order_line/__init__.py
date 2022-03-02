5261ac7cd53a67baf4527dcc3b29db824288aeff
Jordi Ballester <jordi.ballester@forgeflow.com>
2022-03-02 14:51:41 +0100

    [FIX] account_move_line_rma_order_line: Force to trigger again the init hook. As the script was incorrect in previous versions.

--- account_move_line_rma_order_line/__init__.py
+++ account_move_line_rma_order_line/__init__.py
@@ -14,19 +14,19 @@ def post_init_hook(cr, registry):
     aml_moves = aml_model.search([("rma_line_id", "!=", False)])
     sm_moves = sm_model.search([("rma_line_id", "!=", False)])
     for account_move in aml_moves.mapped("move_id"):
-        for aml_w_rma in account_move.invoice_line_ids.filtered(
+        for aml_w_rma in account_move.line_ids.filtered(
             lambda x: x.product_id
             and x.account_id.id
             != x.product_id.categ_id.property_stock_valuation_account_id.id
             and x.rma_line_id
         ):
-            invoice_lines_without_rma = account_move.invoice_line_ids.filtered(
+            move_lines_without_rma = account_move.line_ids.filtered(
                 lambda x: x.product_id.id == aml_w_rma.product_id.id
                 and not x.rma_line_id
                 and aml_w_rma.name in x.name
             )
-            if invoice_lines_without_rma:
-                invoice_lines_without_rma.write(
+            if move_lines_without_rma:
+                move_lines_without_rma.write(
                     {
                         "rma_line_id": aml_w_rma.rma_line_id.id,
                     }
@@ -37,5 +37,6 @@ def post_init_hook(cr, registry):
             for aml in current_layers.mapped("account_move_id.line_ids").filtered(
                 lambda x: x.account_id.id
                 != move.product_id.categ_id.property_stock_valuation_account_id.id
+                and not x.rma_line_id
             ):
                 aml.rma_line_id = move.rma_line_id.id
