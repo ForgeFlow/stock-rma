0b29054c7f75ecaf1217f8b1aa163261f34870ef
Jordi Ballester <jordi.ballester@forgeflow.com>
2022-03-02 11:35:08 +0100

    [IMP] rma: Refactor all rma modules in order to consider using the correct price unit in moves Otherwise the inventory accounting will be completely wrong.

--- rma_account/models/__init__.py
+++ rma_account/models/__init__.py
@@ -1,7 +1,5 @@
-# Copyright 2017 ForgeFlow S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
-
 from . import rma_order
 from . import rma_order_line
 from . import rma_operation
 from . import account_move
+from . import procurement
