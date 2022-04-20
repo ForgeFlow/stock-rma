80ea8e3c24d4c1731af551d220e6c4762a20591c
Andrea <a.stirpe@onestein.nl>
2022-04-20 16:34:53 +0200

    [13.0][FIX/IMP] Make RMA Operation settings company dependent

--- rma/models/product.py
+++ rma/models/product.py
@@ -8,11 +8,14 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     rma_customer_operation_id = fields.Many2one(
+        company_dependent=True,
         comodel_name="rma.operation", string="Default RMA Customer Operation"
     )
     rma_supplier_operation_id = fields.Many2one(
+        company_dependent=True,
         comodel_name="rma.operation", string="Default RMA Supplier Operation"
     )
     rma_approval_policy = fields.Selection(
         related="categ_id.rma_approval_policy", readonly=True
     )
+
