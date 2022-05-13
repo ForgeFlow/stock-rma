222262add08f5c3ee5f944d1cde1c2cbbd2fb68b
AaronHForgeFlow <ahenriquez@eficent.com>
2022-05-13 11:16:10 +0200

    [IMP]rma_account: include features from account_move_line_rma_order_line module from 14.0

--- rma_account/__manifest__.py
+++ rma_account/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "RMA Account",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "license": "LGPL-3",
     "category": "RMA",
     "summary": "Integrates RMA with Invoice Processing",
--- rma_account/models/__init__.py
+++ rma_account/models/__init__.py
@@ -2,4 +2,6 @@ from . import rma_order
 from . import rma_order_line
 from . import rma_operation
 from . import account_move
+from . import account_move_line
 from . import procurement
+from . import stock_move
