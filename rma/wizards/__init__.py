58e43ae0e39940b266ede0122303cf7b88c4373f
Lois Rilo <lois.rilo@forgeflow.com>
2022-05-23 18:39:20 +0200

    [IMP] rma: add rma lines to group selecting serial numbers

--- rma/wizards/__init__.py
+++ rma/wizards/__init__.py
@@ -1,6 +1,4 @@
-# Copyright (C) 2017-20 ForgeFlow S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
-
 from . import rma_add_stock_move
 from . import rma_make_picking
 from . import rma_order_line_make_supplier_rma
+from . import rma_add_serial
