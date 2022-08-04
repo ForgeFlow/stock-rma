3b6b1890857698211000e7e792e4910c0751dc29
Jordi Ballester <jordi.ballester@forgeflow.com>
2022-08-04 08:48:34 +0200

    [ADD] rma_repair_refurbish: split refurbish dependency from rma_repair

---
+++ rma_repair_refurbish/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020-22 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "RMA Repair Refurbish",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "RMA",
+    "summary": "Links RMA with Repairs and Refurbish.",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/ForgeFlow/stock-rma",
+    "depends": ["rma_repair", "repair_refurbish"],
+    "data": [
+        "wizards/rma_order_line_make_repair_view.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
