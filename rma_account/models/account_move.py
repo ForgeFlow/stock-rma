e1e4e1e3e35e5cdd904de05825f2ada47903a7bb
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2022-11-23 20:12:34 +0100

    [FIX] rma_account: Auto-reconcile supplier refunds

--- rma_account/models/account_move.py
+++ rma_account/models/account_move.py
@@ -139,9 +139,18 @@ class AccountMove(models.Model):
             rslt += invoice.mapped("line_ids.rma_line_id.move_ids").filtered(
                 lambda x: x.state == "done" and x.location_dest_id.usage == "customer"
             )
-        for invoice in self.filtered(lambda x: x.move_type == "out_refund"):
             # Add refunds generated from the RMA
+        for invoice in self.filtered(
+            lambda x: x.move_type in ("out_invoice", "out_refund")
+        ):
             rslt += invoice.mapped("line_ids.rma_line_id.move_ids").filtered(
                 lambda x: x.state == "done" and x.location_id.usage == "customer"
+            )
+        for invoice in self.filtered(
+            lambda x: x.move_type in ("in_invoice", "in_refund")
+        ):
+            # Add refunds generated from the RMA
+            rslt += invoice.mapped("line_ids.rma_line_id.move_ids").filtered(
+                lambda x: x.state == "done" and x.location_dest_id.usage == "supplier"
             )
         return rslt
