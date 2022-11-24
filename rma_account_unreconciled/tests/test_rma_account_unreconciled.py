59a94da9bcd65b2d53ce1d18b3dfb4614efbd913
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2022-11-24 18:59:36 +0100

    fix pylint

--- rma_account_unreconciled/tests/test_rma_account_unreconciled.py
+++ rma_account_unreconciled/tests/test_rma_account_unreconciled.py
@@ -54,8 +54,8 @@ class TestRmaAccountUnreconciled(TestRma):
             rma_line.action_rma_approve()
             self.assertFalse(rma_line.unreconciled)
         self.rma_customer_id.rma_line_ids.action_rma_to_approve()
-        wizard = self.rma_make_picking.with_context(
-            {
+        wizard = self.rma_make_pickingwith_context(
+            **{
                 "active_ids": self.rma_customer_id.rma_line_ids.ids,
                 "active_model": "rma.order.line",
                 "picking_type": "incoming",
@@ -73,7 +73,7 @@ class TestRmaAccountUnreconciled(TestRma):
             rma_line._compute_unreconciled()
             self.assertTrue(rma_line.unreconciled)
         make_refund = self.rma_refund_wiz.with_context(
-            {
+            **{
                 "customer": True,
                 "active_ids": self.rma_customer_id.rma_line_ids.ids,
                 "active_model": "rma.order.line",
