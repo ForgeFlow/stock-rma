942192342c1e950913fad18fdbca068d085f90fb
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2022-11-29 08:07:01 +0100

    [IMP] calculate refund unit price

--- rma_sale/wizards/rma_refund.py
+++ rma_sale/wizards/rma_refund.py
@@ -12,6 +12,29 @@ class RmaRefund(models.TransientModel):
         res["sale_line_id"] = line.sale_line_id.id
         return res
 
+    def _get_refund_price_unit(self, rma):
+        price_unit = super(RmaRefund, self)._get_refund_price_unit(rma)
+        if rma.type == "customer":
+            if rma.account_move_line_id:
+                price_unit = rma.account_move_line_id.price_unit
+            elif rma.sale_line_id:
+                price_unit = rma.sale_line_id.price_unit
+            else:
+                # Fall back to the sale price if no reference is found.
+                price_unit = rma.product_id.with_company(rma.company_id).lst_price
+        return price_unit
+
+    def _get_refund_currency(self, rma):
+        currency = rma.currency_id
+        if rma.type == "customer":
+            if rma.account_move_line_id:
+                currency = rma.account_move_line_id.currency_id
+            elif rma.sale_line_id:
+                currency = rma.sale_line_id.currency_id
+            else:
+                currency = rma.company_id.currency_id
+        return currency
+
 
 class RmaRefundItem(models.TransientModel):
     _inherit = "rma.refund.item"
