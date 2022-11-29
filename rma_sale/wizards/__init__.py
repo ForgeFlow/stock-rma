bb0acdc0d256212d4036674856dea33f6e401085
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2022-11-29 08:07:01 +0100

    [IMP] calculate refund unit price

--- rma_sale/wizards/__init__.py
+++ rma_sale/wizards/__init__.py
@@ -1,6 +1,3 @@
-# Copyright 2020 ForgeFlow S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
-
 from . import rma_order_line_make_sale_order
 from . import rma_make_picking
 from . import rma_refund
