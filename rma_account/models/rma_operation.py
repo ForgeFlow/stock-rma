009d8951c605d77e0dcdeb6b9702e664f2415cf3
AaronHForgeFlow <ahenriquez@eficent.com>
2023-01-04 11:57:36 +0100

    [14.0][FIX] rma_account: reconcile GDNI when receiving but refund is expected

--- rma_account/models/rma_operation.py
+++ rma_account/models/rma_operation.py
@@ -27,6 +27,16 @@ class RmaOperation(models.Model):
         comodel_name="account.journal",
         compute="_compute_domain_valid_journal",
     )
+    automated_refund = fields.Boolean(
+        help="In the scenario where a company uses anglo-saxon accounting, if "
+        "you receive products from a customer and don't expect to refund the customer "
+        "but send a replacement unit, mark this flag to be accounting consistent"
+    )
+    refund_free_of_charge = fields.Boolean(
+        help="In case of automated refund you should mark this option as long automated"
+        "refunds mean to compensate Stock Interim accounts only without hitting"
+        "Accounts receivable"
+    )
 
     @api.onchange("type")
     def _compute_domain_valid_journal(self):
@@ -39,3 +49,9 @@ class RmaOperation(models.Model):
                 rec.valid_refund_journal_ids = self.env["account.journal"].search(
                     [("type", "=", "purchase")]
                 )
+
+    @api.onchange("automated_refund")
+    def _onchange_automated_refund(self):
+        for rec in self:
+            if rec.automated_refund:
+                rec.refund_free_of_charge = True
