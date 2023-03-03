fdd80e64e3f2a393da6abcf892df7683f91eff58
DavidJForgeFlow <david.jimenez@forgeflow.com>
2023-03-03 16:09:09 +0100

    [MIG] rma_sale_force_invoiced: Migration to 15.0

--- rma_sale_force_invoiced/__manifest__.py
+++ rma_sale_force_invoiced/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "RMA Sale Force Invoiced",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "category": "RMA",
     "summary": "Forces sales orders created from RMA to be forced as invoiced",
