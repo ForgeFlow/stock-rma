d5633df0393b1bf3f1dadaff06ba2a0ef552a18f
Juany Davila <juanyadavilap@gmail.com>
2023-03-22 16:03:02 -0500

    [IMP] rma_sale_analytic: black, isort, prettier

--- rma_sale_analytic/wizards/rma_add_sale.py
+++ rma_sale_analytic/wizards/rma_add_sale.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
@@ -6,11 +5,10 @@ from odoo import api, models
 
 
 class RmaAddSale(models.TransientModel):
-    _inherit = 'rma_add_sale'
+    _inherit = "rma_add_sale"
 
     @api.model
     def _prepare_rma_line_from_sale_order_line(self, line):
-        data = super(RmaAddSale, self).\
-            _prepare_rma_line_from_sale_order_line(line)
+        data = super(RmaAddSale, self)._prepare_rma_line_from_sale_order_line(line)
         data.update(analytic_account_id=line.analytic_account_id.id)
         return data
--- rma_sale_analytic/wizards/rma_order_line_make_sale_order.py
+++ rma_sale_analytic/wizards/rma_order_line_make_sale_order.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # © 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
-from odoo import models, api
+from odoo import api, models
 
 
 class RmaLineMakeSaleOrder(models.TransientModel):
@@ -10,8 +9,6 @@ class RmaLineMakeSaleOrder(models.TransientModel):
 
     @api.model
     def _prepare_sale_order(self, line):
-        res = super(
-            RmaLineMakeSaleOrder, self)._prepare_sale_order(line)
-        res.update(
-            project_id=line.analytic_account_id.id)
+        res = super(RmaLineMakeSaleOrder, self)._prepare_sale_order(line)
+        res.update(project_id=line.analytic_account_id.id)
         return res
