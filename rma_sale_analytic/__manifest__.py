c19418d95361ac3a477f9386608adf6bd34ae7a9
Juany Davila <juanyadavilap@gmail.com>
2023-03-22 16:03:02 -0500

    [IMP] rma_sale_analytic: black, isort, prettier

--- rma_sale_analytic/__manifest__.py
+++ rma_sale_analytic/__manifest__.py
@@ -1,17 +1,14 @@
-# -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Analytic Account in RMA sale",
     "version": "10.0.1.0.0",
-    "author": "Eficent,"
-              "Odoo Community Association (OCA)",
+    "author": "Eficent," "Odoo Community Association (OCA)",
     "license": "LGPL-3",
-    "website": "http://www.eficent.com",
+    "website": "https://github.com/ForgeFlow/stock-rma",
     "category": "Analytic",
     "depends": ["rma_account", "rma_analytic"],
-    "data": [
-    ],
-    'installable': True,
+    "data": [],
+    "installable": True,
 }
