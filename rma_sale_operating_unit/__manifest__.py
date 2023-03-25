5297a9aacaec64809db5f7ee8f120d01b2f8519d
Juany Davila <juanyadavilap@gmail.com>
2023-03-25 14:29:46 -0500

    [MIG] rma_sale_operating_unit: migration to 15.0

--- rma_sale_operating_unit/__manifest__.py
+++ rma_sale_operating_unit/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Analytic Account in RMA sale",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow," "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "website": "https://github.com/ForgeFlow/stock-rma",
