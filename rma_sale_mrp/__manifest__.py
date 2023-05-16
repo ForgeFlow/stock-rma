dfc7e0845c13d4af6f65147b803200b16532d2d5
Christopher Ormaza <chris.ormaza@forgeflow.com>
2023-05-16 15:01:23 -0500

    [14.0][ADD] rma_sale_mrp

---
+++ rma_sale_mrp/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 ForgeFlow S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+{
+    "name": "RMA Manufacturing Integration",
+    "version": "14.0.1.1.0",
+    "license": "LGPL-3",
+    "category": "RMA",
+    "summary": "Integrates RMA with Kit Manufacturing/Sale Process",
+    "author": "ForgeFlow",
+    "website": "https://github.com/ForgeFlow/stock-rma",
+    "depends": ["rma_sale", "sale_mrp"],
+    "data": [],
+    "installable": True,
+}
