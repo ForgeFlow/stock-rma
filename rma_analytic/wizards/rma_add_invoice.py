c7285604b41ed3462e432b0d1d7c3125276e5bba
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-07-18 16:28:37 +0530

    [IMP] rma_analytic: black, isort, prettier

--- rma_analytic/wizards/rma_add_invoice.py
+++ rma_analytic/wizards/rma_add_invoice.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
@@ -9,11 +8,7 @@ class RmaAddInvoice(models.TransientModel):
     _inherit = "rma_add_invoice"
 
     def _prepare_rma_line_from_inv_line(self, line):
-        res = super(
-            RmaAddInvoice, self
-        )._prepare_rma_line_from_inv_line(line)
+        res = super(RmaAddInvoice, self)._prepare_rma_line_from_inv_line(line)
         if line.account_analytic_id:
-            res.update(
-                analytic_account_id=line.account_analytic_id.id
-            )
+            res.update(analytic_account_id=line.account_analytic_id.id)
         return res
