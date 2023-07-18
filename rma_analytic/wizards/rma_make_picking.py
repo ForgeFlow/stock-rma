bebf88e2515de629dec7c7221e98ac453920eb06
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-07-18 18:03:27 +0530

    [MIG] rma_analytic: Migration to 15.0

--- rma_analytic/wizards/rma_make_picking.py
+++ rma_analytic/wizards/rma_make_picking.py
@@ -1,4 +1,4 @@
-# © 2018 Eficent Business and IT Consulting Services S.L.
+# Copyright 2023 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 from odoo import api, models
@@ -6,12 +6,11 @@ from odoo import api, models
 
 class RmaMakePicking(models.TransientModel):
     _inherit = "rma_make_picking.wizard"
-    _description = "Wizard to create pickings from rma lines"
 
     @api.model
     def _get_procurement_data(self, item, group, qty, picking_type):
         procurement_data = super(RmaMakePicking, self)._get_procurement_data(
             item, group, qty, picking_type
         )
-        procurement_data.update(account_analytic_id=item.line_id.analytic_account_id.id)
+        procurement_data.update(analytic_account_id=item.line_id.analytic_account_id.id)
         return procurement_data
