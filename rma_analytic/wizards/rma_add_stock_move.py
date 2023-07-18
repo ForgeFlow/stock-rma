cdf617e15950addc04fae905ecf370a2bf72f421
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-07-18 16:28:37 +0530

    [IMP] rma_analytic: black, isort, prettier

--- rma_analytic/wizards/rma_add_stock_move.py
+++ rma_analytic/wizards/rma_add_stock_move.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
@@ -6,12 +5,11 @@ from odoo import api, models
 
 
 class RmaAddStockMove(models.TransientModel):
-    _inherit = 'rma_add_stock_move'
-    _description = 'Wizard to add rma lines from pickings'
+    _inherit = "rma_add_stock_move"
+    _description = "Wizard to add rma lines from pickings"
 
     @api.model
     def _prepare_rma_line_from_stock_move(self, sm, lot=False):
-        data = super(RmaAddStockMove, self)._prepare_rma_line_from_stock_move(
-            sm, lot)
+        data = super(RmaAddStockMove, self)._prepare_rma_line_from_stock_move(sm, lot)
         data.update(analytic_account_id=sm.analytic_account_id.id)
         return data
