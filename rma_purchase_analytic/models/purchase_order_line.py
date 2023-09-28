622ce019e7cbf4de8345bb750e282fea2ec1d1b4
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-09-28 08:44:54 +0530

    [IMP] rma_purchase_analytic: black, isort, prettier

--- rma_purchase_analytic/models/purchase_order_line.py
+++ rma_purchase_analytic/models/purchase_order_line.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
@@ -8,11 +7,13 @@ from odoo import _, api, exceptions, models
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
-    @api.constrains('account_analytic_id')
+    @api.constrains("account_analytic_id")
     def check_analytic(self):
         for line in self.filtered(lambda p: p.rma_line_id):
-            if (line.account_analytic_id !=
-                    line.rma_line_id.analytic_account_id):
+            if line.account_analytic_id != line.rma_line_id.analytic_account_id:
                 raise exceptions.ValidationError(
-                    _("The analytic account in the PO line it's not the same"
-                      " as in the rma line"))
+                    _(
+                        "The analytic account in the PO line it's not the same"
+                        " as in the rma line"
+                    )
+                )
