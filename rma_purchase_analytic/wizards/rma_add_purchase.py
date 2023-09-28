510bc20b413519333d14b5d129b446b8b1d4ddb9
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-09-28 08:44:54 +0530

    [IMP] rma_purchase_analytic: black, isort, prettier

--- rma_purchase_analytic/wizards/rma_add_purchase.py
+++ rma_purchase_analytic/wizards/rma_add_purchase.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2018 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
@@ -6,7 +5,7 @@ from odoo import api, models
 
 
 class RmaAddPurchase(models.TransientModel):
-    _inherit = 'rma_add_purchase'
+    _inherit = "rma_add_purchase"
 
     @api.model
     def _prepare_rma_line_from_po_line(self, line):
@@ -21,8 +20,8 @@ class RmaLineMakePurchaseOrder(models.TransientModel):
 
     @api.model
     def _prepare_purchase_order_line(self, po, item):
-        res = super(
-            RmaLineMakePurchaseOrder, self)._prepare_purchase_order_line(
-            po, item)
-        res['account_analytic_id'] = item.line_id.analytic_account_id.id
+        res = super(RmaLineMakePurchaseOrder, self)._prepare_purchase_order_line(
+            po, item
+        )
+        res["account_analytic_id"] = item.line_id.analytic_account_id.id
         return res
