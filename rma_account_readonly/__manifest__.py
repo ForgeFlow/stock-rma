cc0e61ff8785085c0c379c3e0ed580a645cfa838
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2023-10-25 17:18:25 +0200

    [ADD] rma_account_readonly in v15

---
+++ rma_account_readonly/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "RMA Account Readonly",
+    "version": "15.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "RMA",
+    "summary": "RMA account readonly access",
+    "author": "ForgeFlow",
+    "website": "https://github.com/ForgeFlow/stock-rma",
+    "depends": ["rma_account", "rma_readonly"],
+    "data": [
+        "views/rma_menus.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
