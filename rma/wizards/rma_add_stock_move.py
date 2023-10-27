3507dcd6fcf35edd6441c63bbeaa1765c0f80ced
ChrisOForgeFlow <94866688+ChrisOForgeFlow@users.noreply.github.com>
2023-10-27 07:41:54 -0500

    [14.0][IMP] added default operation on rma group, easy setup before rma lines created (#452)
    * [14.0][IMP] added default operation on rma group, easy setup before rma lines created
    
    * [IMP] added fields for default route created by wizard on rma group
    
    * fix: get right price after create rma order line

--- rma/wizards/rma_add_stock_move.py
+++ rma/wizards/rma_add_stock_move.py
@@ -73,16 +73,18 @@ class RmaAddStockMove(models.TransientModel):
         }
 
     def _prepare_rma_line_from_stock_move(self, sm, lot=False):
-        if self.env.context.get("customer"):
-            operation = (
-                sm.product_id.rma_customer_operation_id
-                or sm.product_id.categ_id.rma_customer_operation_id
-            )
-        else:
-            operation = (
-                sm.product_id.rma_supplier_operation_id
-                or sm.product_id.categ_id.rma_supplier_operation_id
-            )
+        operation = self.rma_id.operation_default_id
+        if not operation:
+            if self.env.context.get("customer"):
+                operation = (
+                    sm.product_id.rma_customer_operation_id
+                    or sm.product_id.categ_id.rma_customer_operation_id
+                )
+            else:
+                operation = (
+                    sm.product_id.rma_supplier_operation_id
+                    or sm.product_id.categ_id.rma_supplier_operation_id
+                )
         if not operation:
             operation = self.env["rma.operation"].search(
                 [("type", "=", self.rma_id.type)], limit=1
@@ -96,8 +98,11 @@ class RmaAddStockMove(models.TransientModel):
             )
             if not route:
                 raise ValidationError(_("Please define an RMA route"))
-
-        if not operation.in_warehouse_id or not operation.out_warehouse_id:
+        in_warehouse = self.rma_id.in_warehouse_id or operation.in_warehouse_id
+        in_route = self.rma_id.in_route_id or operation.in_route_id
+        out_warehouse = self.rma_id.out_warehouse_id or operation.out_warehouse_id
+        out_route = self.rma_id.out_route_id or operation.out_route_id
+        if not in_warehouse or not out_warehouse:
             warehouse = self.env["stock.warehouse"].search(
                 [
                     ("company_id", "=", self.rma_id.company_id.id),
@@ -109,6 +114,16 @@ class RmaAddStockMove(models.TransientModel):
                 raise ValidationError(
                     _("Please define a warehouse with a default RMA location")
                 )
+            in_warehouse = in_warehouse or warehouse
+            out_warehouse = out_warehouse or warehouse
+        location = self.rma_id.location_id
+        if not location:
+            location = (
+                operation.location_id
+                or operation.in_warehouse_id.lot_rma_id
+                or in_warehouse.lot_rma_id
+                or out_warehouse.lot_rma_id
+            )
         product_qty = sm.product_uom_qty
         if sm.product_id.tracking == "serial":
             product_qty = 1
@@ -132,15 +147,11 @@ class RmaAddStockMove(models.TransientModel):
             "rma_id": self.rma_id.id,
             "receipt_policy": operation.receipt_policy,
             "delivery_policy": operation.delivery_policy,
-            "in_warehouse_id": operation.in_warehouse_id.id or warehouse.id,
-            "out_warehouse_id": operation.out_warehouse_id.id or warehouse.id,
-            "in_route_id": operation.in_route_id.id or route.id,
-            "out_route_id": operation.out_route_id.id or route.id,
-            "location_id": (
-                operation.location_id.id
-                or operation.in_warehouse_id.lot_rma_id.id
-                or warehouse.lot_rma_id.id
-            ),
+            "in_warehouse_id": in_warehouse.id,
+            "out_warehouse_id": out_warehouse.id,
+            "in_route_id": in_route.id,
+            "out_route_id": out_route.id,
+            "location_id": location.id,
         }
         return data
 
@@ -184,4 +195,5 @@ class RmaAddStockMove(models.TransientModel):
                         #  favor of (pre)computed stored editable fields for all policies
                         #  and configuration in the RMA operation.
                         rec._onchange_operation_id()
+                        rec.price_unit = rec._get_price_unit()
         return {"type": "ir.actions.act_window_close"}
