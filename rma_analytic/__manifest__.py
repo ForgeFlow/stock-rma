1a09c5e35b9ddf71aea65cc5c01cf1224934d7d3
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2023-10-30 15:08:32 +0100

    [MIG] rma_analytic to v16

--- rma_analytic/__manifest__.py
+++ rma_analytic/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Analytic Account in RMA",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "ForgeFlow," "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/ForgeFlow/stock-rma",
