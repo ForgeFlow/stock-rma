c25a64bb30111af2e8e420be0f1b4d63ea97c966
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2023-10-31 12:59:36 +0100

    [ADD] rma_account_product_warranty in v16

---
+++ rma_account_product_warranty/models/rma_order_line.py
@@ -0,0 +1,34 @@
+# Copyright 2023 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from dateutil.relativedelta import relativedelta
+
+from odoo import models
+
+
+class RmaOrderLine(models.Model):
+
+    _inherit = "rma.order.line"
+
+    def _compute_warranty_end_date(self):
+        res = super()._compute_warranty_end_date()
+        for rec in self:
+            warranty = rec.product_id.warranty
+            if rec.account_move_line_id and warranty:
+                if rec.product_id.warranty_type == "day":
+                    rec.warranty_end_date = (
+                        rec.account_move_line_id.date + relativedelta(days=warranty)
+                    )
+                elif rec.product_id.warranty_type == "week":
+                    rec.warranty_end_date = (
+                        rec.account_move_line_id.date + relativedelta(weeks=warranty)
+                    )
+                elif rec.product_id.warranty_type == "month":
+                    rec.warranty_end_date = (
+                        rec.account_move_line_id.date + relativedelta(months=warranty)
+                    )
+                elif rec.product_id.warranty_type == "year":
+                    rec.warranty_end_date = (
+                        rec.account_move_line_id.date + relativedelta(years=warranty)
+                    )
+        return res
