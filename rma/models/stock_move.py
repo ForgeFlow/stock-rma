97c03f6551aeb2f19333e21a1523d77f096f7a34
Christopher Ormaza <chris.ormaza@forgeflow.com>
2024-01-31 15:32:36 -0500

    [14.0][FIX] rma, add support to handle package used on customer moves

--- rma/models/stock_move.py
+++ rma/models/stock_move.py
@@ -111,3 +111,15 @@ class StockMove(models.Model):
         res = super()._prepare_procurement_values()
         res["rma_line_id"] = self.rma_line_id.id
         return res
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    def _should_bypass_reservation(self, location):
+        res = super()._should_bypass_reservation(location)
+        if self.env.context.get(
+            "force_no_bypass_reservation"
+        ) and location.usage not in ("customer", "supplier"):
+            return False
+        return res
