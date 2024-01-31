1a2ca0aae9f1951b2956c02da1a992e0305b09f0
Christopher Ormaza <chris.ormaza@forgeflow.com>
2024-01-31 15:32:36 -0500

    [14.0][FIX] rma, add support to handle package used on customer moves

--- rma/models/rma_order_line.py
+++ rma/models/rma_order_line.py
@@ -482,6 +482,10 @@ class RmaOrderLine(models.Model):
     )
     under_warranty = fields.Boolean(string="Under Warranty?")
 
+    def _get_stock_move_reference(self):
+        self.ensure_one()
+        return self.reference_move_id
+
     def _prepare_rma_line_from_stock_move(self, sm, lot=False):
         if not self.type:
             self.type = self._get_default_type()
