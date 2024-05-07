c23b8053e596f394958012662fa0aaf43b2b440f
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2024-05-07 18:03:27 +0200

    [FIX] rma_purchase: name_search migration

--- rma_purchase/models/purchase_order_line.py
+++ rma_purchase/models/purchase_order_line.py
@@ -2,70 +2,57 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 from odoo import api, fields, models
+from odoo.osv import expression
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
+    _rec_names_search = ["name", "order_id"]
 
     rma_line_id = fields.Many2one(
         comodel_name="rma.order.line",
         string="RMA",
     )
 
-    @api.model
-    def name_search(self, name="", args=None, operator="ilike", limit=100):
-        """Allows to search by PO reference."""
-        if not args:
-            args = []
-        args += [
-            "|",
-            (self._rec_name, operator, name),
-            ("order_id.name", operator, name),
-        ]
-        return super().name_search(name=name, args=args, operator=operator, limit=limit)
+    def _name_search(self, name, domain=None, operator="ilike", limit=None, order=None):
+        domain = domain or []
+        if self.env.context.get("rma"):
+            domain = expression.AND([domain, [("display_type", "=", False)]])
+        lines = self.search([("order_id.name", operator, name)] + domain, limit=limit)
+        if limit:
+            limit_rest = limit - len(lines)
+        else:
+            # limit can be 0 or None representing infinite
+            limit_rest = limit
+        if limit_rest or not limit:
+            domain += [("id", "in", lines.ids)]
+            return super()._name_search(
+                name, domain=domain, operator=operator, limit=limit_rest, order=order
+            )
+        return self._search(domain, limit=limit, order=order)
 
-    @api.model
-    def _name_search(
-        self, name="", args=None, operator="ilike", limit=100, name_get_uid=None
-    ):
-        """Typed text is cleared here for better extensibility."""
-        return super()._name_search(
-            name="",
-            args=args,
-            operator=operator,
-            limit=limit,
-            name_get_uid=name_get_uid,
+    def _get_purchase_line_rma_name_get_label(self):
+        self.ensure_one()
+        return "PO:{} | INV: {}, | PART:{} | QTY:{}".format(
+            self.order_id.name,
+            " ".join(str(x) for x in [inv.name for inv in self.order_id.invoice_ids]),
+            self.product_id.name,
+            self.product_uom_qty,
         )
 
-    def name_get(self):
-        res = []
-        if self.env.context.get("rma"):
-            for purchase in self:
-                invoices = self.env["account.move.line"].search(
-                    [("purchase_line_id", "=", purchase.id)]
-                )
-                if purchase.order_id.name:
-                    res.append(
-                        (
-                            purchase.id,
-                            "{} {} {} qty:{}".format(
-                                purchase.order_id.name,
-                                " ".join(
-                                    str(x)
-                                    for x in [
-                                        inv.name for inv in invoices.mapped("move_id")
-                                    ]
-                                ),
-                                purchase.product_id.name,
-                                purchase.product_qty,
-                            ),
-                        )
+    def _compute_display_name(self):
+        if not self.env.context.get("rma"):
+            return super()._compute_display_name()
+        for purchase_line in self:
+            for purchase_line in self:
+                if purchase_line.order_id.name:
+                    purchase_line.display_name = (
+                        purchase_line._get_purchase_line_rma_name_get_label()
                     )
                 else:
-                    res.append(super(PurchaseOrderLine, purchase).name_get()[0])
-            return res
-        else:
-            return super().name_get()
+                    return super(
+                        PurchaseOrderLine, purchase_line
+                    )._compute_display_name()
 
     @api.model_create_multi
     def create(self, vals_list):
