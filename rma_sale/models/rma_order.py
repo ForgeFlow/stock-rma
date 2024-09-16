3c88ceea258b10335dd028aa01ecff54218ec5a3
chafique.delli <chafique.delli@akretion.com>
2024-09-16 12:33:05 +0200

    [ADD] smart button for sales in rma group

--- rma_sale/models/rma_order.py
+++ rma_sale/models/rma_order.py
@@ -16,6 +16,16 @@ class RmaOrder(models.Model):
             src_sales = rma.mapped("rma_line_ids.sale_line_id.order_id")
             rma.src_sale_count = len(src_sales)
 
+    @api.depends(
+        "rma_line_ids",
+        "rma_line_ids.sale_line_ids",
+        "rma_line_ids.sale_line_ids.order_id",
+    )
+    def _compute_sale_count(self):
+        for rma in self:
+            sales = rma.mapped("rma_line_ids.sale_line_ids.order_id")
+            rma.sale_count = len(sales)
+
     @api.depends("rma_line_ids", "rma_line_ids.qty_to_sell")
     def _compute_qty_to_sell(self):
         for rec in self:
@@ -28,6 +38,7 @@ class RmaOrder(models.Model):
         digits="Product Unit of Measure",
         compute="_compute_qty_to_sell",
     )
+    sale_count = fields.Integer(compute="_compute_sale_count", string="# of Sales")
 
     @api.model
     def _get_line_domain(self, rma_id, line):
@@ -41,9 +52,16 @@ class RmaOrder(models.Model):
             domain = super(RmaOrder, self)._get_line_domain(rma_id, line)
         return domain
 
+    def action_view_origin_sale_order(self):
+        action = self.env.ref("sale.action_orders")
+        result = action.sudo().read()[0]
+        so_ids = self.mapped("rma_line_ids.sale_line_id.order_id").ids
+        result["domain"] = [("id", "in", so_ids)]
+        return result
+
     def action_view_sale_order(self):
         action = self.env.ref("sale.action_quotations")
         result = action.sudo().read()[0]
-        so_ids = self.mapped("rma_line_ids.sale_line_id.order_id").ids
+        so_ids = self.mapped("rma_line_ids.sale_line_ids.order_id").ids
         result["domain"] = [("id", "in", so_ids)]
         return result
