48c2fedd9efe9c9533bf171934da640ce18dbf35
Florian da Costa <florian.dacosta@akretion.com>
2024-11-07 16:17:11 +0100

    [FIX] rma: qty received and shipped in case of multiple step reception/expedition Now the moves are filtered and only 1 move of the chain is selected to compute the quantity (the first one for reception and last one for expedition), so we should no longer ignore those to compute the quantities

--- rma/models/rma_order_line.py
+++ rma/models/rma_order_line.py
@@ -125,11 +125,6 @@ class RmaOrderLine(models.Model):
                     )
                 )
             for move in moves:
-                # If the move is part of a chain don't count it
-                if direction == "out" and move.move_orig_ids:
-                    continue
-                elif direction == "in" and move.move_dest_ids:
-                    continue
                 qty += product_obj._compute_quantity(move.product_uom_qty, rec.uom_id)
             return qty
 
@@ -184,7 +179,7 @@ class RmaOrderLine(models.Model):
     def _compute_qty_outgoing(self):
         for rec in self:
             qty = rec._get_rma_move_qty(
-                ("draft", "confirmed", "assigned"), direction="out"
+                ("draft", "confirmed", "assigned", "waiting"), direction="out"
             )
             rec.qty_outgoing = qty
 
