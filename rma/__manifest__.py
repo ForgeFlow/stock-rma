6854f0134f0f769384feb6aec3f70568501dcbad
DavidJForgeFlow <david.jimenez@forgeflow.com>
2024-12-10 16:01:17 +0100

    [FIX] rma
    Avoid reserving units that are not available. Also ensures the lot picked is the correct one by adding a dependency to stock_move_forced_lot.

--- rma/__manifest__.py
+++ rma/__manifest__.py
@@ -10,7 +10,7 @@
     "in odoo",
     "author": "ForgeFlow",
     "website": "https://github.com/ForgeFlow/stock-rma",
-    "depends": ["stock", "mail", "web"],
+    "depends": ["stock", "mail", "web", "stock_move_forced_lot"],
     "demo": ["demo/stock_demo.xml"],
     "data": [
         "security/rma.xml",
