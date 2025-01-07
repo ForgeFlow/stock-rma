d709c962ddf04a81b1415a46cb7c260ce212189b
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2025-01-07 12:35:45 +0530

    [IMP] rma_repair: black, isort, prettier

--- rma_repair/__manifest__.py
+++ rma_repair/__manifest__.py
@@ -7,7 +7,7 @@
     "category": "RMA",
     "summary": "Links RMA with Repairs.",
     "author": "ForgeFlow",
-    "website": "https://github.com/ForgeFlow/stock-rma",
+    "website": "https://github.com/ForgeFlow",
     "depends": ["rma_account", "repair"],
     "data": [
         "security/ir.model.access.csv",
