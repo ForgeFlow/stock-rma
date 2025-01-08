2684d83bf3ffba0b292d1e308ef943926394bb27
DavidJForgeFlow <david.jimenez@forgeflow.com>
2025-01-08 12:38:16 +0100

    [MIG] rma_filter_lot: Migration to 17.0
    Moved the domain from the python declaration to xml view field to match base changes (https://github.com/ForgeFlow/stock-rma/commit/6c67346b084c7764e2904b247b6c9b3dc0598cd8).

--- rma_filter_lot/__manifest__.py
+++ rma_filter_lot/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2021 ForgeFlow S.L.
+# Copyright 2021-24 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 {
     "name": "RMA Filter Lot",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "LGPL-3",
     "category": "RMA",
     "summary": "Filter RMA lots",
--- rma_filter_lot/models/rma_order_line.py
+++ rma_filter_lot/models/rma_order_line.py
@@ -1,4 +1,4 @@
-# Copyright 2021 ForgeFlow S.L.
+# Copyright 2021-24 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 from odoo import api, fields, models
@@ -8,27 +8,22 @@ from odoo.tools import float_compare
 class RmaOrderLine(models.Model):
     _inherit = "rma.order.line"
 
-    lot_id = fields.Many2one(
-        domain="[('id', 'in', valid_lot_ids)]",
-    )
     valid_lot_ids = fields.One2many(
-        comodel_name="stock.production.lot",
+        comodel_name="stock.lot",
         compute="_compute_domain_lot_ids",
     )
 
     @api.depends("product_id")
     def _compute_domain_lot_ids(self):
         for rec in self:
-            lots = rec.env["stock.production.lot"].search(
-                [("product_id", "=", rec.product_id.id)]
-            )
+            lots = rec.env["stock.lot"].search([("product_id", "=", rec.product_id.id)])
             if (
                 lots
                 and rec.type == "customer"
                 and rec.product_id
                 and rec.product_id.tracking != "none"
             ):
-                valid_ids = self.env["stock.production.lot"]
+                valid_ids = self.env["stock.lot"]
                 for quant in rec.product_id.stock_quant_ids:
                     if (
                         float_compare(quant.available_quantity, 0.0, precision_digits=2)
