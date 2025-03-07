e15057409c08f7b4e5ad755e08308b6c07e03b3f
Florian da Costa <florian.dacosta@akretion.com>
2025-03-07 12:17:18 +0100

    [REF] Force reservation of rma lot only for reception of the current rma lines processed
    This avoid to remove reservation from previously generated stock.move
    Also avoid creating stock.move line with a reserved quantity to 0 for deliveries

--- rma/wizards/rma_make_picking.py
+++ rma/wizards/rma_make_picking.py
@@ -203,9 +203,57 @@ class RmaMakePicking(models.TransientModel):
             procurements.extend(procurement)
         return procurements
 
+    def _force_reservation_specific_lot(self, pickings, picking_type):
+        move_line_model = self.env["stock.move.line"]
+        # Force the reservation of the RMA specific lot for incoming shipments.
+        # FIXME: still needs fixing, not reserving appropriate serials.
+        if picking_type == "incoming":
+            rma_line_candidates = self.item_ids.line_id
+            for move in pickings.move_ids.filtered(
+                lambda x: x.state not in ("draft", "cancel", "done", "waiting")
+                and x.rma_line_id
+                and x.rma_line_id in rma_line_candidates
+                and x.product_id.tracking in ("lot", "serial")
+                and x.rma_line_id.lot_id
+            ):
+                # Force the reservation of the RMA specific lot for incoming shipments.
+                move.move_line_ids.unlink()
+                if move.product_id.tracking == "serial":
+                    move.write(
+                        {
+                            "lot_ids": [(6, 0, move.rma_line_id.lot_id.ids)],
+                        }
+                    )
+                    quants = self.env["stock.quant"]._gather(
+                        move.product_id,
+                        move.location_id,
+                        lot_id=move.rma_line_id.lot_id,
+                    )
+                    move.move_line_ids.write(
+                        {
+                            "reserved_uom_qty": 1,
+                            "qty_done": 0,
+                            "package_id": len(quants) == 1 and quants.package_id.id,
+                        }
+                    )
+                elif move.product_id.tracking == "lot":
+                    qty = self.item_ids.filtered(
+                        lambda x: x.line_id.id == move.rma_line_id.id
+                    ).qty_to_receive
+                    move_line_data = move._prepare_move_line_vals()
+                    move_line_data.update(
+                        {
+                            "lot_id": move.rma_line_id.lot_id.id,
+                            "product_uom_id": move.product_id.uom_id.id,
+                            "qty_done": 0,
+                            "reserved_uom_qty": qty,
+                        }
+                    )
+                    move_line_model.create(move_line_data)
+                move.with_context(force_no_bypass_reservation=True)._action_assign()
+
     def action_create_picking(self):
         self._create_picking()
-        move_line_model = self.env["stock.move.line"]
         picking_type = self.env.context.get("picking_type")
         if picking_type == "outgoing":
             pickings = self.mapped("item_ids.line_id")._get_out_pickings()
@@ -213,52 +261,7 @@ class RmaMakePicking(models.TransientModel):
         else:
             pickings = self.mapped("item_ids.line_id")._get_in_pickings()
             action = self.item_ids.line_id.action_view_in_shipments()
-        # Force the reservation of the RMA specific lot for incoming shipments.
-        # FIXME: still needs fixing, not reserving appropriate serials.
-        for move in pickings.move_ids.filtered(
-            lambda x: x.state not in ("draft", "cancel", "done", "waiting")
-            and x.rma_line_id
-            and x.product_id.tracking in ("lot", "serial")
-            and x.rma_line_id.lot_id
-        ):
-            # Force the reservation of the RMA specific lot for incoming shipments.
-            move.move_line_ids.unlink()
-            if move.product_id.tracking == "serial":
-                move.write(
-                    {
-                        "lot_ids": [(6, 0, move.rma_line_id.lot_id.ids)],
-                    }
-                )
-                quants = self.env["stock.quant"]._gather(
-                    move.product_id, move.location_id, lot_id=move.rma_line_id.lot_id
-                )
-                move.move_line_ids.write(
-                    {
-                        "reserved_uom_qty": 1 if picking_type == "incoming" else 0,
-                        "qty_done": 0,
-                        "package_id": len(quants) == 1 and quants.package_id.id,
-                    }
-                )
-            elif move.product_id.tracking == "lot":
-                if picking_type == "incoming":
-                    qty = self.item_ids.filtered(
-                        lambda x: x.line_id.id == move.rma_line_id.id
-                    ).qty_to_receive
-                else:
-                    qty = self.item_ids.filtered(
-                        lambda x: x.line_id.id == move.rma_line_id.id
-                    ).qty_to_deliver
-                move_line_data = move._prepare_move_line_vals()
-                move_line_data.update(
-                    {
-                        "lot_id": move.rma_line_id.lot_id.id,
-                        "product_uom_id": move.product_id.uom_id.id,
-                        "qty_done": 0,
-                        "reserved_uom_qty": qty if picking_type == "incoming" else 0,
-                    }
-                )
-                move_line_model.create(move_line_data)
-            pickings.with_context(force_no_bypass_reservation=True).action_assign()
+        self._force_reservation_specific_lot(pickings, picking_type)
         return action
 
     def action_cancel(self):
