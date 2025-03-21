3c8e49abd9a30936988f69dd0ed284b12970547f
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2025-03-21 09:38:47 +0530

    [IMP] rma: Fix calculation for qty_to_receive

--- rma/models/rma_order_line.py
+++ rma/models/rma_order_line.py
@@ -129,6 +129,7 @@ class RmaOrderLine(models.Model):
         "move_ids",
         "move_ids.state",
         "qty_received",
+        "qty_incoming",
         "receipt_policy",
         "product_qty",
         "type",
@@ -137,9 +138,13 @@ class RmaOrderLine(models.Model):
         for rec in self:
             rec.qty_to_receive = 0.0
             if rec.receipt_policy == "ordered":
-                rec.qty_to_receive = rec.product_qty - rec.qty_received
+                rec.qty_to_receive = (
+                    rec.product_qty - rec.qty_incoming - rec.qty_received
+                )
             elif rec.receipt_policy == "delivered":
-                rec.qty_to_receive = rec.qty_delivered - rec.qty_received
+                rec.qty_to_receive = (
+                    rec.qty_delivered - rec.qty_incoming - rec.qty_received
+                )
 
     @api.depends(
         "move_ids",
