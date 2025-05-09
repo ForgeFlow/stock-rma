94b8a7cd9ed41c46388e9d139f0cc6181e46f650
ArnauCForgeFlow <arnau.cruz@forgeflow.com>
2025-05-09 13:22:17 +0200

    [IMP] rma: remove forced lot assignment in RMA moves

--- rma/wizards/rma_make_picking.py
+++ rma/wizards/rma_make_picking.py
@@ -280,13 +280,6 @@ class RmaMakePicking(models.TransientModel):
                     "quantity": 1.0,
                 }
             )
-            if move.rma_line_id.lot_id and not quants:
-                # CHECK ME: force al least has lot assigned if quant is not found
-                move_line_data.update(
-                    {
-                        "lot_id": move.rma_line_id.lot_id.id,
-                    }
-                )
             if move.product_id.tracking == "serial":
                 move.write(
                     {
